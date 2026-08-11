3b87742736740497af4eec50af04f72dfc5fffcc
vhdtshop-ux <vhdtshop@gmail.com>
2026-08-11 10:31:54 +0700

    feat(website): define phase 1B permission catalog

--- Modules/Website/Config/module.php
+++ Modules/Website/Config/module.php
@@ -14,9 +14,34 @@ return array (
   ),
   'permissions' => 
   array (
+    // Legacy permissions retained for backward compatibility.
     0 => 'view_website',
     1 => 'create_website',
     2 => 'edit_website',
     3 => 'delete_website',
+
+    // Website capabilities.
+    4 => 'website.view',
+    5 => 'website.home.manage',
+    6 => 'website.menu.manage',
+    7 => 'website.footer.manage',
+    8 => 'website.banner.manage',
+    9 => 'website.settings.manage',
+
+    // Marketing capabilities currently hosted by Website.
+    10 => 'marketing.coupon.view',
+    11 => 'marketing.coupon.manage',
+    12 => 'marketing.flash-sale.view',
+    13 => 'marketing.flash-sale.manage',
+
+    // Customer capabilities currently hosted by Website.
+    14 => 'customer.view',
+    15 => 'customer.create',
+    16 => 'customer.update',
+    17 => 'customer.delete',
+
+    // Affiliate capabilities.
+    18 => 'affiliate.view',
+    19 => 'affiliate.manage',
   ),
 );
