987930a4e2909d041c48b989f6f78bc065edc331
vhdtshop-ux <vhdtshop@gmail.com>
2026-08-12 15:27:56 +0700

    test(invoices): provision sqlite schema for import regression

--- tests/Feature/InvoicesModuleTest.php
+++ tests/Feature/InvoicesModuleTest.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Bus;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Schema;
 use Livewire\Livewire;
 use Modules\Invoices\Jobs\ProcessGdtInvoicesJob;
 use Modules\Invoices\Livewire\GdtInvoice;
@@ -103,6 +104,9 @@ class InvoicesModuleTest extends TestCase
 
     public function test_excel_import_supports_sold_and_purchase(): void
     {
+        $migration = require base_path('Modules/Invoices/database/migrations/2025_11_21_045614_invoices.php');
+        $migration->up();
+
         $file = storage_path('app/invoices-module-test.xlsx');
         (new FastExcel([[
             'Mã tra cứu' => 'test-lookup',
@@ -144,6 +148,7 @@ class InvoicesModuleTest extends TestCase
                 ->assertSee('Test');
         } finally {
             DB::rollBack();
+            Schema::dropIfExists('invoices');
 
             if (file_exists($file)) {
                 unlink($file);
