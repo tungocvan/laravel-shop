b41d9f472d2616277ff35573a9a2b1b6785f9efd
vhdtshop-ux <vhdtshop@gmail.com>
2026-08-12 15:09:48 +0700

    fix(admin): restore route permission contracts

--- Modules/Admin/routes/web.php
+++ Modules/Admin/routes/web.php
@@ -8,33 +8,36 @@ use Modules\Admin\Http\Controllers\ProfileController;
 
 Route::middleware(['web', 'auth:admin'])->prefix('admin')->name('admin.')->group(function () {
     Route::get('/', [DashboardController::class, 'index'])
-        ->middleware('permission:view_admin,admin')
+        ->middleware('permission:admin.dashboard.view,admin')
         ->name('dashboard');
 
-    Route::prefix('menus')->middleware('permission:create_admin,admin')->name('menus.')->group(function () {
+    Route::prefix('menus')->name('menus.')->group(function () {
         Route::get('/', [MenuController::class, 'index'])
+            ->middleware('permission:admin.menu.view,admin')
             ->name('index');
 
         Route::get('/create', [MenuController::class, 'create'])
+            ->middleware('permission:admin.menu.create,admin')
             ->name('create');
 
         Route::get('/{id}/edit', [MenuController::class, 'edit'])
+            ->middleware('permission:admin.menu.update,admin')
             ->name('edit');
     });
 
     Route::get('/profile', [ProfileController::class, 'profile'])
-        ->middleware('permission:view_admin,admin')
+        ->middleware('permission:admin.profile.view,admin')
         ->name('profile');
 
     Route::get('/themes', [AdminController::class, 'themes'])
-        ->middleware('permission:view_admin,admin')
+        ->middleware('permission:admin.theme.view,admin')
         ->name('themes');
 
     Route::get('/layout', [AdminController::class, 'layout'])
-        ->middleware('permission:create_admin,admin')
+        ->middleware('permission:admin.layout.view,admin')
         ->name('layout');
 
     Route::get('/admin-header', [AdminController::class, 'adminHeader'])
-        ->middleware('permission:create_admin,admin')
+        ->middleware('permission:admin.header.view,admin')
         ->name('header');
 });
