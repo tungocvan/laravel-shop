29875fc25422332c02c8a9a74bb219b48bb5df9b
vhdtshop-ux <vhdtshop@gmail.com>
2026-08-12 14:17:45 +0700

    fix(system): restore StorageConfig env contract

---
+++ Modules/System/Livewire/Settings/StorageConfig.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Modules\System\Livewire\Settings;
+
+use Livewire\Component;
+
+class StorageConfig extends Component
+{
+    public function render()
+    {
+        return view('System::livewire.settings.storage-config');
+    }
+}
