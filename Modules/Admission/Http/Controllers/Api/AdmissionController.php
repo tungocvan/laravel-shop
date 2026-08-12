845ca0b4e8a8f4242586f2625820d167a291e85a
vhdtshop-ux <vhdtshop@gmail.com>
2026-08-12 15:14:37 +0700

    fix(admission): restore route and api contracts

--- Modules/Admission/Http/Controllers/Api/AdmissionController.php
+++ Modules/Admission/Http/Controllers/Api/AdmissionController.php
@@ -3,9 +3,14 @@
 namespace Modules\Admission\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
 
 class AdmissionController extends Controller
 {
-    //
+    public function index(): JsonResponse
+    {
+        return response()->json([
+            'message' => 'Admission API is not available yet.',
+        ], 501);
+    }
 }
