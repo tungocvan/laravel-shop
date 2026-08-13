c7a33097179c133dd9e59db9d2d6874edc374c88
vhdtshop-ux <vhdtshop@gmail.com>
2026-08-13 10:32:14 +0700

    feat(system): expose queue manager settings tab

--- Modules/System/config/system_tabs.php
+++ Modules/System/config/system_tabs.php
@@ -38,9 +38,17 @@ return [
     ],
 
     [
+        'id' => 'queues',
+        'label' => 'Queue Manager',
+        'icon' => 'M4 6h16M4 12h16M4 18h16',
+        'component' => 'system.settings.queue-manager',
+        'enabled' => true,
+    ],
+
+    [
         'id' => 'mail',
         'label' => 'Cấu hình Email',
-        'icon' => 'M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z',
+        'icon' => 'M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7M5 5h14a2 2 0 012 2v10a2 2 0 01-2 2H5a2 2 0 01-2-2V7a2 2 0 012-2z',
         'component' => 'system.settings.mail-config',
         'enabled' => true,
     ],
@@ -60,4 +68,3 @@ return [
     ],   
 
 ];
- 
