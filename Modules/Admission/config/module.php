e75ee35eeb3c215c23cdf074e19c1af732d01ef5
vhdtshop-ux <vhdtshop@gmail.com>
2026-08-13 08:48:20 +0700

    feat(admission): configure optional PDF conversion from env

--- Modules/Admission/config/module.php
+++ Modules/Admission/config/module.php
@@ -4,7 +4,7 @@ return array (
   'name' => 'Admission',
   'type' => 'domain',
   'enabled' => true,
-  'enable_pdf_convert' => false,
+  'enable_pdf_convert' => env('ENABLE_PDF_CONVERT', false),
   'seeders' => 
   array (
     0 => 'Modules\\Admission\\database\\seeders\\DatabaseSeeder',
