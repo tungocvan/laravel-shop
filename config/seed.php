6a5c376acb11944bdf10f33e6eb44d114c7812da
vhdtshop-ux <vhdtshop@gmail.com>
2026-08-14 15:48:40 +0700

    config(seed): add admin seeding credentials

---
+++ config/seed.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'admin' => [
+        'name' => env('SEED_ADMIN_NAME', 'Từ Ngọc Vân'),
+        'email' => env('SEED_ADMIN_EMAIL', 'tungocvan@gmail.com'),
+        'password' => env('SEED_ADMIN_PASSWORD'),
+    ],
+];
