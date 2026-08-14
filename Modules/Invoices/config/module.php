fda57bc018592d0645d2766ba17fc07018ce4d44
vhdtshop-ux <vhdtshop@gmail.com>
2026-08-14 15:35:56 +0700

    feat(invoices): declare permission catalog

--- Modules/Invoices/config/module.php
+++ Modules/Invoices/config/module.php
@@ -1,14 +1,18 @@
 <?php
 
-return array (
-  'name' => 'Invoices',
-  'type' => 'domain',
-  'enabled' => true,
-  'depends' => 
-  array (
-  ),
-  'tables' => 
-  array (
-    0 => 'invoices',
-  ),
-);
+return [
+    'name' => 'Invoices',
+    'type' => 'domain',
+    'enabled' => true,
+    'depends' => [],
+    'tables' => [
+        'invoices',
+    ],
+    'permissions' => [
+        'invoices-list',
+        'invoices-create',
+        'invoices-export',
+        'invoices-download',
+        'invoices-configure',
+    ],
+];
