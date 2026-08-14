1adb3e257545a77fe9de14e05953de1c0586984a
vhdtshop-ux <vhdtshop@gmail.com>
2026-08-14 15:39:00 +0700

    feat(order): declare permission catalog

--- Modules/Order/config/module.php
+++ Modules/Order/config/module.php
@@ -1,12 +1,19 @@
 <?php
 
-return array (
-  'name' => 'Order',
-  'type' => 'domain',
-  'enabled' => true,
-  'depends' => 
-  array (
-    0 => 'User',
-    1 => 'Product',
-  ),
-);
+return [
+    'name' => 'Order',
+    'type' => 'domain',
+    'enabled' => true,
+    'depends' => [
+        'User',
+        'Product',
+    ],
+    'permissions' => [
+        'orders-list',
+        'orders-view',
+        'orders-update-status',
+        'orders-delete',
+        'orders-print',
+        'orders-export-pdf',
+    ],
+];
