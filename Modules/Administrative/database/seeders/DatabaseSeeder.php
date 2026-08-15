f81325b076552f8c98fada8099ce1c3542b642ef
vhdtshop-ux <vhdtshop@gmail.com>
2026-08-15 09:26:46 +0700

    include Administrative demo data in module seeder

--- Modules/Administrative/database/seeders/DatabaseSeeder.php
+++ Modules/Administrative/database/seeders/DatabaseSeeder.php
@@ -11,6 +11,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             ProcedureSeeder::class,
             AdministrativeMenuSeeder::class,
+            AdministrativeDemoSeeder::class,
         ]);
     }
 }
