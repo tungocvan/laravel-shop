6201d3cb0569f72f4927887a55751c039c587099
vhdtshop-ux <vhdtshop@gmail.com>
2026-08-16 16:18:20 +0700

    test(muasamcong): include contractor history route in authorization coverage

--- tests/Feature/Muasamcong/MuasamcongRouteAuthorizationTest.php
+++ tests/Feature/Muasamcong/MuasamcongRouteAuthorizationTest.php
@@ -11,13 +11,16 @@ class MuasamcongRouteAuthorizationTest extends TestCase
     {
         $index = Route::getRoutes()->getByName('muasamcong.index');
         $hsmt = Route::getRoutes()->getByName('muasamcong.hsmt');
+        $contractors = Route::getRoutes()->getByName('muasamcong.contractors');
 
         $this->assertNotNull($index);
         $this->assertNotNull($hsmt);
+        $this->assertNotNull($contractors);
         $this->assertSame('admin/muasamcong', $index->uri());
         $this->assertSame('admin/muasamcong/hsmt', $hsmt->uri());
+        $this->assertSame('admin/muasamcong/contractors', $contractors->uri());
 
-        foreach ([$index, $hsmt] as $route) {
+        foreach ([$index, $hsmt, $contractors] as $route) {
             $middleware = $route->gatherMiddleware();
             $this->assertContains('auth:admin', $middleware);
             $this->assertContains('permission:view_muasamcong,admin', $middleware);
@@ -43,14 +46,16 @@ class MuasamcongRouteAuthorizationTest extends TestCase
             ->filter(fn (string $uri): bool => str_contains($uri, 'muasamcong'))
             ->values();
 
-        $this->assertCount(5, $uris);
+        $this->assertCount(6, $uris);
         $this->assertContains('api/muasamcong', $uris);
         $this->assertContains('api/muasamcong/search-pricing', $uris);
         $this->assertContains('admin/muasamcong', $uris);
         $this->assertContains('admin/muasamcong/hsmt', $uris);
+        $this->assertContains('admin/muasamcong/contractors', $uris);
         $this->assertContains('admin/muasamcong/config', $uris);
         $this->assertNotContains('muasamcong', $uris);
         $this->assertNotContains('muasamcong/hsmt', $uris);
+        $this->assertNotContains('muasamcong/contractors', $uris);
         $this->assertNotContains('muasamcong/config', $uris);
     }
 }
