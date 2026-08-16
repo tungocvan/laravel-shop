80cb19c31c9eaaa2de7de43421e8a3fa7373b10b
vhdtshop-ux <vhdtshop@gmail.com>
2026-08-16 15:25:25 +0700

    test(muasamcong): cover direct contract winner schema

--- tests/Feature/Muasamcong/KqlcntServiceTest.php
+++ tests/Feature/Muasamcong/KqlcntServiceTest.php
@@ -90,6 +90,7 @@ class KqlcntServiceTest extends TestCase
         $this->assertSame('vn2000269822', $result['investor_code']);
         $this->assertSame('Sở Y tế Cà Mau', $result['investor_name']);
         $this->assertCount(1, $result['contracts']);
+        $this->assertCount(2, $result['all_winners']);
         $this->assertSame('73/2026/SYT-CM', $result['contracts'][0]['contractNo']);
         $this->assertSame('CÔNG TY FP', $result['contracts'][0]['contractorPassListParsed'][0]['contractorName']);
         $this->assertSame([], $result['verified_lots']);
@@ -137,5 +138,58 @@ class KqlcntServiceTest extends TestCase
 
         $this->assertCount(1, $result['verified_lots']);
         $this->assertSame('PP2500561639', $result['verified_lots'][0]['lotNo']);
+    }
+
+    public function test_kqlcnt_reads_winner_from_direct_contract_fields_when_pass_list_is_null(): void
+    {
+        config()->set('muasamcong.smart_token', 'test-token');
+        config()->set('muasamcong.verify_ssl', true);
+        config()->set('muasamcong.endpoints.kqlcnt_tbmt_detail', 'https://muasamcong.mpi.gov.vn/o/egp-portal-contractor-selection-v2/services/lcnt_tbmt_ttc_ldt');
+        config()->set('muasamcong.endpoints.kqlcnt_contracts', 'https://muasamcong.mpi.gov.vn/o/egp-portal-contractor-selection-v2/services/econsign/contract-info/list-contract-for-po');
+        config()->set('muasamcong.referers.kqlcnt', 'https://muasamcong.mpi.gov.vn/web/guest/contractor-selection');
+
+        Http::fake(function ($request) {
+            if (str_contains($request->url(), 'lcnt_tbmt_ttc_ldt')) {
+                return Http::response([
+                    'bidoNotifyContractorM' => [
+                        'bidName' => 'Gói thuốc Generic',
+                        'isMedicine' => 1,
+                        'investorName' => 'Bệnh viện A',
+                    ],
+                    'bidoBidStatus' => ['status' => 'PUB_KQLCNT', 'published' => 1],
+                ]);
+            }
+
+            return Http::response([
+                [
+                    'contractNo' => '218/HĐNT',
+                    'contractorCode' => 'vn0402010696',
+                    'contractorName' => 'CÔNG TY CỔ PHẦN DƯỢC PHẨM NAM SƠN - NAMPHACO',
+                    'contractorPassList' => null,
+                    'lotResultDTO' => json_encode([['listTablePrice' => []]]),
+                ],
+                [
+                    'contractNo' => '219/HĐNT',
+                    'contractorCode' => 'vn0309829522',
+                    'contractorName' => 'CÔNG TY CỔ PHẦN GONSA',
+                    'contractorPassList' => null,
+                    'lotResultDTO' => json_encode([['listTablePrice' => []]]),
+                ],
+            ]);
+        });
+
+        $result = app(KqlcntService::class)->resolve(
+            '316e2f33-d8fe-4d9a-b020-b7068aa9398d',
+            'IB2600008930',
+            'vn0402010696'
+        );
+
+        $this->assertTrue($result['current_contractor_won']);
+        $this->assertCount(1, $result['contracts']);
+        $this->assertCount(2, $result['all_winners']);
+        $this->assertSame('218/HĐNT', $result['contracts'][0]['contractNo']);
+        $this->assertSame('vn0402010696', $result['contracts'][0]['contractorPassListParsed'][0]['contractorCode']);
+        $this->assertSame('CÔNG TY CỔ PHẦN DƯỢC PHẨM NAM SƠN - NAMPHACO', $result['all_winners'][0]['contractorName']);
+        $this->assertSame([], $result['verified_lots']);
     }
 }
