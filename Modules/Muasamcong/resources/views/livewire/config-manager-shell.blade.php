13585c1802183db77ad290cb79d821c9bc7259b4
vhdtshop-ux <vhdtshop@gmail.com>
2026-08-16 20:02:49 +0700

    feat(muasamcong): expose personal session manager in config

--- Modules/Muasamcong/resources/views/livewire/config-manager-shell.blade.php
+++ Modules/Muasamcong/resources/views/livewire/config-manager-shell.blade.php
@@ -1,4 +1,5 @@
 <div class="space-y-5">
     @include('Muasamcong::livewire.partials.environment-doctor')
+    @include('Muasamcong::livewire.partials.personal-session-manager')
     @include('Muasamcong::livewire.config-manager')
 </div>
