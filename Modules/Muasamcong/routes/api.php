ae26c9ba67e04fa687472cf81de7d236419729ed
vhdtshop-ux <vhdtshop@gmail.com>
2026-08-17 09:38:39 +0700

    feat(muasamcong): expose one-time session import POST

--- Modules/Muasamcong/routes/api.php
+++ Modules/Muasamcong/routes/api.php
@@ -2,10 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use Modules\Muasamcong\Http\Controllers\Api\MuasamcongController;
+use Modules\Muasamcong\Http\Controllers\Api\PersonalSessionImportController;
 
-// Route::middleware('auth:sanctum')->controller(MuasamcongController::class)->prefix('muasamcong')->group(function(){
-//         Route::get('/', 'index');
-// });
+Route::post('muasamcong/update-cookie', PersonalSessionImportController::class)
+    ->middleware(['api', 'throttle:6,1'])
+    ->name('muasamcong.session-import');
 
 Route::middleware(config('muasamcong.api_middleware', ['api', 'auth:sanctum']))->prefix('muasamcong')->controller(MuasamcongController::class)->group(function () {
     Route::get('/', 'index');
