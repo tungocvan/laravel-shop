e9fc9ef02e611b440a9b0ce3b922460369210fb2
vhdtshop-ux <vhdtshop@gmail.com>
2026-08-18 15:20:32 +0700

    feat(muasamcong): add synced export profile model

---
+++ Modules/Muasamcong/Models/SyncedExportProfile.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Modules\Muasamcong\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class SyncedExportProfile extends Model
+{
+    protected $table = 'muasamcong_synced_export_profiles';
+
+    protected $guarded = [];
+
+    protected $casts = [
+        'is_default' => 'boolean',
+        'column_order' => 'array',
+        'selected_columns' => 'array',
+        'headers' => 'array',
+        'alignments' => 'array',
+        'widths' => 'array',
+        'data_types' => 'array',
+    ];
+}
