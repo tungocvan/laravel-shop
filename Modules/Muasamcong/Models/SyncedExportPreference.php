ef25b49a1d426596d717890112d69b39534ad10d
vhdtshop-ux <vhdtshop@gmail.com>
2026-08-18 14:29:50 +0700

    feat(muasamcong): add synced export preference model

---
+++ Modules/Muasamcong/Models/SyncedExportPreference.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Modules\Muasamcong\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class SyncedExportPreference extends Model
+{
+    protected $table = 'muasamcong_synced_export_preferences';
+
+    protected $guarded = [];
+
+    protected $casts = [
+        'column_order' => 'array',
+        'selected_columns' => 'array',
+        'alignments' => 'array',
+    ];
+}
