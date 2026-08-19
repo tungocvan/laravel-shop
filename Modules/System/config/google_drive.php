3d865817f8ef733bd0d6544a67e6b4f5909c3f60
vhdtshop-ux <vhdtshop@gmail.com>
2026-08-19 12:51:08 +0700

    feat(system): derive Google Drive callback from app URL

--- Modules/System/config/google_drive.php
+++ Modules/System/config/google_drive.php
@@ -1,9 +1,14 @@
 <?php
 
+$explicitRedirectUri = trim((string) env('GOOGLE_DRIVE_REDIRECT_URI', ''));
+$appUrl = rtrim(trim((string) env('APP_URL', '')), '/');
+
 return [
     'client_id' => env('GOOGLE_DRIVE_CLIENT_ID', ''),
     'client_secret' => env('GOOGLE_DRIVE_CLIENT_SECRET', ''),
-    'redirect_uri' => env('GOOGLE_DRIVE_REDIRECT_URI', ''),
+    'redirect_uri' => $explicitRedirectUri !== ''
+        ? $explicitRedirectUri
+        : ($appUrl !== '' ? $appUrl.'/admin/system/settings/cloud/google/callback' : ''),
     'folder_name' => env('GOOGLE_DRIVE_FOLDER_NAME', 'Laravel-Backup'),
     'scopes' => [
         'openid',
