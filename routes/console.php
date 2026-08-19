549fe648bf1034b02e99caf340f35362429247ae
vhdtshop-ux <vhdtshop@gmail.com>
2026-08-19 12:17:38 +0700

    chore(scheduler): gate Facebook scheduler behind env flag

--- routes/console.php
+++ routes/console.php
@@ -8,9 +8,11 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-Schedule::command('facebook:dispatch-scheduled')
-    ->everyMinute()
-    ->withoutOverlapping();
+if (filter_var(env('FACEBOOK_SCHEDULER_ENABLED', false), FILTER_VALIDATE_BOOL)) {
+    Schedule::command('facebook:dispatch-scheduled')
+        ->everyMinute()
+        ->withoutOverlapping();
+}
 
 Schedule::command('system:cloud-backup')
     ->everyMinute()
