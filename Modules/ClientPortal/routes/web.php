fd996dea8c1fe0ce465a2e4310480b5a3008061a
vhdtshop-ux <vhdtshop@gmail.com>
2026-08-19 15:26:03 +0700

    refactor(client-portal): auto-load application adapter routes

--- Modules/ClientPortal/routes/web.php
+++ Modules/ClientPortal/routes/web.php
@@ -1,28 +1,12 @@
 <?php
 
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Route;
-use Modules\ClientPortal\Applications\Muasamcong\Http\Controllers\MuasamcongApplicationController;
 use Modules\ClientPortal\Http\Controllers\Admin\ApplicationAdminController;
 use Modules\ClientPortal\Http\Controllers\PortalController;
 
 Route::middleware(['web', 'auth:web'])->group(function () {
     Route::get('/my-apps', [PortalController::class, 'index'])->name('client.apps.index');
-
-    Route::middleware('client.application:muasamcong')
-        ->prefix('apps/muasamcong')
-        ->name('client.muasamcong.')
-        ->group(function () {
-            Route::get('/', [MuasamcongApplicationController::class, 'dashboard'])->name('dashboard');
-            Route::get('/drug-pricing', [MuasamcongApplicationController::class, 'drugPricing'])
-                ->middleware('client.feature:muasamcong,drug-pricing')
-                ->name('drug-pricing');
-            Route::get('/drug-pricing/{sourceId}', [MuasamcongApplicationController::class, 'drugPricingDetail'])
-                ->middleware('client.feature:muasamcong,drug-pricing')
-                ->name('drug-pricing.detail');
-            Route::post('/drug-pricing/sync', [MuasamcongApplicationController::class, 'queueDrugPricingSync'])
-                ->middleware('client.feature:muasamcong,drug-pricing')
-                ->name('drug-pricing.sync');
-        });
 });
 
 Route::middleware(['web', 'auth:admin'])->prefix('admin/client-apps')->name('admin.client-apps.')->group(function () {
@@ -34,3 +18,7 @@ Route::middleware(['web', 'auth:admin'])->prefix('admin/client-apps')->name('adm
     Route::get('/roles/{role}', [ApplicationAdminController::class, 'editRole'])->middleware('permission:edit_role,admin')->name('roles.edit');
     Route::put('/roles/{role}', [ApplicationAdminController::class, 'updateRole'])->middleware('permission:edit_role,admin')->name('roles.update');
 });
+
+foreach (File::glob(base_path('Modules/ClientPortal/Applications/*/routes.php')) as $applicationRoutes) {
+    require $applicationRoutes;
+}
