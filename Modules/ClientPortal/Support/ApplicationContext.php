d0850388b2f3469bc02ee87fd446e3de5cb991c6
vhdtshop-ux <vhdtshop@gmail.com>
2026-08-19 15:13:46 +0700

    refactor(client-portal): move application context into module

---
+++ Modules/ClientPortal/Support/ApplicationContext.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Modules\ClientPortal\Support;
+
+class ApplicationContext
+{
+    private ?array $application = null;
+
+    public function set(array $application): void
+    {
+        $this->application = $application;
+    }
+
+    public function current(): ?array
+    {
+        return $this->application;
+    }
+
+    public function key(): ?string
+    {
+        return $this->application['key'] ?? null;
+    }
+
+    public function clear(): void
+    {
+        $this->application = null;
+    }
+}
