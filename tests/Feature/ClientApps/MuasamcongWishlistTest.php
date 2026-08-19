89cd0c649a979d1f7a7b221e678616c8410f51e0
vhdtshop-ux <vhdtshop@gmail.com>
2026-08-19 19:14:10 +0700

    test(client-portal): cover inline wishlist toggle route

--- tests/Feature/ClientApps/MuasamcongWishlistTest.php
+++ tests/Feature/ClientApps/MuasamcongWishlistTest.php
@@ -10,7 +10,7 @@ class MuasamcongWishlistTest extends TestCase
 {
     public function test_wishlist_routes_require_client_application_and_feature(): void
     {
-        foreach (['client.muasamcong.wishlist','client.muasamcong.wishlist.store','client.muasamcong.wishlist.destroy'] as $name) {
+        foreach (['client.muasamcong.wishlist','client.muasamcong.wishlist.store','client.muasamcong.wishlist.toggle','client.muasamcong.wishlist.destroy'] as $name) {
             $route=Route::getRoutes()->getByName($name);
             $this->assertNotNull($route);
             $middleware=$route->gatherMiddleware();
