5305299dba3a9a54e63fc3a7721a0d0c52debcc4
vhdtshop-ux <vhdtshop@gmail.com>
2026-08-19 16:47:58 +0700

    feat(client-portal): route public share management

--- Modules/ClientPortal/Applications/Muasamcong/routes.php
+++ Modules/ClientPortal/Applications/Muasamcong/routes.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use Modules\ClientPortal\Applications\Muasamcong\Http\Controllers\MuasamcongApplicationController;
 use Modules\ClientPortal\Applications\Muasamcong\Http\Controllers\MuasamcongHistoryController;
+use Modules\ClientPortal\Applications\Muasamcong\Http\Controllers\MuasamcongShareManagementController;
 use Modules\ClientPortal\Applications\Muasamcong\Http\Controllers\PublicDrugShareController;
 
 if ((bool) config('modules.registry.Muasamcong.enabled', false)) {
@@ -21,7 +22,9 @@ if ((bool) config('modules.registry.Muasamcong.enabled', false)) {
             Route::middleware('client.feature:muasamcong,drug-pricing')->group(function () {
                 Route::get('/drug-pricing', [MuasamcongApplicationController::class, 'drugPricing'])->name('drug-pricing');
                 Route::post('/drug-pricing/share', [PublicDrugShareController::class, 'store'])->name('drug-pricing.share');
-                Route::delete('/drug-pricing/share/{share}', [PublicDrugShareController::class, 'revoke'])->name('drug-pricing.share.revoke');
+                Route::get('/shares', [MuasamcongShareManagementController::class, 'index'])->name('shares');
+                Route::patch('/shares/{share}/expiry', [MuasamcongShareManagementController::class, 'updateExpiry'])->name('shares.expiry');
+                Route::delete('/shares/{share}', [MuasamcongShareManagementController::class, 'revoke'])->name('shares.revoke');
                 Route::get('/drug-pricing/sync/{syncRequest}/status', [MuasamcongApplicationController::class, 'drugPricingSyncStatus'])
                     ->whereUuid('syncRequest')
                     ->name('drug-pricing.sync-status');
