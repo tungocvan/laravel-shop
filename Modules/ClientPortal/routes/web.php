079b680d135c9819a3308699d25ab3ab6b168be8
tungocvan <tungocvan@gmail.com>
2026-08-20 15:34:22 +0700

    feat(clientportal): add PWA admin settings routes

--- Modules/ClientPortal/routes/web.php
+++ Modules/ClientPortal/routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Route;
 use Modules\ClientPortal\Http\Controllers\Admin\ApplicationAdminController;
+use Modules\ClientPortal\Http\Controllers\Admin\PwaSettingsController;
 use Modules\ClientPortal\Http\Controllers\PortalController;
 
 Route::middleware(['web'])->group(function () {
@@ -17,6 +18,11 @@ Route::middleware(['web', 'auth:admin'])->prefix('admin/client-apps')->name('adm
     Route::get('/', [ApplicationAdminController::class, 'index'])->middleware('permission:view_role,admin')->name('index');
     Route::post('/sync-permissions', [ApplicationAdminController::class, 'syncPermissions'])->middleware('permission:edit_role,admin')->name('sync-permissions');
     Route::post('/sync-super-admin', [ApplicationAdminController::class, 'syncSuperAdmin'])->middleware('permission:edit_role,admin')->name('sync-super-admin');
+
+    Route::get('/pwa', [PwaSettingsController::class, 'edit'])->middleware('permission:edit_role,admin')->name('pwa.edit');
+    Route::put('/pwa/general', [PwaSettingsController::class, 'updateGeneral'])->middleware('permission:edit_role,admin')->name('pwa.general.update');
+    Route::put('/pwa/login', [PwaSettingsController::class, 'updateLogin'])->middleware('permission:edit_role,admin')->name('pwa.login.update');
+
     Route::get('/users/{user}', [ApplicationAdminController::class, 'editUser'])->middleware('permission:edit_user,admin')->name('users.edit');
     Route::put('/users/{user}', [ApplicationAdminController::class, 'updateUser'])->middleware('permission:edit_user,admin')->name('users.update');
     Route::get('/roles/{role}', [ApplicationAdminController::class, 'editRole'])->middleware('permission:edit_role,admin')->name('roles.edit');
