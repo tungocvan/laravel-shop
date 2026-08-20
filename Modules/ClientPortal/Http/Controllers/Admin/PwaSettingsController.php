d5710317aa2fcca7673f5b3652366853a5613c43
tungocvan <tungocvan@gmail.com>
2026-08-20 15:35:25 +0700

    chore(clientportal): clean PWA settings controller imports

--- Modules/ClientPortal/Http/Controllers/Admin/PwaSettingsController.php
+++ Modules/ClientPortal/Http/Controllers/Admin/PwaSettingsController.php
@@ -5,7 +5,6 @@ namespace Modules\ClientPortal\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
-use Illuminate\Validation\Rule;
 use Illuminate\View\View;
 use Modules\ClientPortal\Services\ClientPortalSettingsService;
 
