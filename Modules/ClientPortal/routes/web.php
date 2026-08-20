193f5da784c3d5f1f787bb04921534eeb2b56b99
tungocvan <tungocvan@gmail.com>
2026-08-20 16:24:46 +0700

    feat: add ClientPortal application presentation admin routes

--- Modules/ClientPortal/routes/web.php
+++ Modules/ClientPortal/routes/web.php
@@ -8,30 +8,25 @@ use Modules\ClientPortal\Http\Controllers\PortalController;
 
 Route::middleware(['web'])->group(function () {
     Route::get('/my-apps/login', [PortalController::class, 'login'])->name('client.apps.login');
-
-    Route::middleware('auth:web')->group(function () {
-        Route::get('/my-apps', [PortalController::class, 'index'])->name('client.apps.index');
-    });
+    Route::middleware('auth:web')->group(function () { Route::get('/my-apps', [PortalController::class, 'index'])->name('client.apps.index'); });
 });
 
 Route::middleware(['web', 'auth:admin'])->prefix('admin/client-apps')->name('admin.client-apps.')->group(function () {
     Route::get('/', [ApplicationAdminController::class, 'index'])->middleware('permission:view_role,admin')->name('index');
     Route::post('/sync-permissions', [ApplicationAdminController::class, 'syncPermissions'])->middleware('permission:edit_role,admin')->name('sync-permissions');
     Route::post('/sync-super-admin', [ApplicationAdminController::class, 'syncSuperAdmin'])->middleware('permission:edit_role,admin')->name('sync-super-admin');
-
     Route::get('/pwa', [PwaSettingsController::class, 'edit'])->middleware('permission:edit_role,admin')->name('pwa.edit');
     Route::put('/pwa/general', [PwaSettingsController::class, 'updateGeneral'])->middleware('permission:edit_role,admin')->name('pwa.general.update');
     Route::put('/pwa/login', [PwaSettingsController::class, 'updateLogin'])->middleware('permission:edit_role,admin')->name('pwa.login.update');
     Route::get('/pwa/launcher', [PwaSettingsController::class, 'editLauncher'])->middleware('permission:edit_role,admin')->name('pwa.launcher.edit');
     Route::put('/pwa/launcher', [PwaSettingsController::class, 'updateLauncher'])->middleware('permission:edit_role,admin')->name('pwa.launcher.update');
+    Route::get('/pwa/applications/{application}', [PwaSettingsController::class, 'editApplication'])->middleware('permission:edit_role,admin')->name('pwa.applications.edit');
     Route::put('/pwa/applications/{application}', [PwaSettingsController::class, 'updateApplication'])->middleware('permission:edit_role,admin')->name('pwa.applications.update');
-
+    Route::put('/pwa/applications/{application}/features/{feature}', [PwaSettingsController::class, 'updateFeature'])->middleware('permission:edit_role,admin')->name('pwa.features.update');
     Route::get('/users/{user}', [ApplicationAdminController::class, 'editUser'])->middleware('permission:edit_user,admin')->name('users.edit');
     Route::put('/users/{user}', [ApplicationAdminController::class, 'updateUser'])->middleware('permission:edit_user,admin')->name('users.update');
     Route::get('/roles/{role}', [ApplicationAdminController::class, 'editRole'])->middleware('permission:edit_role,admin')->name('roles.edit');
     Route::put('/roles/{role}', [ApplicationAdminController::class, 'updateRole'])->middleware('permission:edit_role,admin')->name('roles.update');
 });
 
-foreach (File::glob(base_path('Modules/ClientPortal/Applications/*/routes.php')) as $applicationRoutes) {
-    require $applicationRoutes;
-}
+foreach (File::glob(base_path('Modules/ClientPortal/Applications/*/routes.php')) as $applicationRoutes) { require $applicationRoutes; }
