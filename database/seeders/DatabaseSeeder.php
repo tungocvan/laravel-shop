fea9e1f1e9dbe72582f511957b3321d9174f1b2d
tungocvan <tungocvan@gmail.com>
2026-08-21 16:50:47 +0700

    Limit root database seeder to core seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,7 +4,6 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\File;
 use Modules\Admin\database\seeders\AdminMenuSeeder;
 use Modules\Role\database\seeders\RolesAndPermissionsSeeder;
 use Modules\User\database\seeders\UserAdminSeeder;
@@ -23,30 +22,5 @@ class DatabaseSeeder extends Seeder
             UserAdminSeeder::class,
             AdminMenuSeeder::class,
         ]);
-
-        $this->seedEnabledModules();
-    }
-
-    private function seedEnabledModules(): void
-    {
-        foreach (config('modules.registry', []) as $name => $module) {
-            if (! ($module['enabled'] ?? false)) {
-                continue;
-            }
-
-            $manifestPath = collect([
-                $module['path'].'/config/module.php',
-                $module['path'].'/Config/module.php',
-            ])->first(fn (string $path): bool => File::exists($path));
-            $manifest = $manifestPath ? require $manifestPath : [];
-
-            foreach ($manifest['seeders'] ?? [] as $seeder) {
-                if (! is_string($seeder) || ! class_exists($seeder)) {
-                    throw new \RuntimeException("Seeder [{$seeder}] của module [{$name}] không tồn tại.");
-                }
-
-                $this->call($seeder);
-            }
-        }
     }
 }
