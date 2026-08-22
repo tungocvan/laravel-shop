63d489efc9e50a510a00e921eb27fbf2039bd9c8
tungocvan <tungocvan@gmail.com>
2026-08-22 16:08:24 +0700

    Extract Website frontend runtime head

---
+++ Modules/Website/resources/views/partials/layout/runtime-head.blade.php
@@ -0,0 +1,14 @@
+{{-- Privileged trusted configuration; mutation requires website.settings.manage. --}}
+{!! $headerScript !!}
+{!! $analyticsCode ?? '' !!}
+
+<script>
+    window.CHAT_CONFIG_HOST = @json(config('realtime.host') ?: request()->getSchemeAndHttpHost());
+</script>
+<x-realtime-config />
+
+@include('Website::partials.design-tokens')
+@vite(['resources/css/tailwind.css', 'resources/js/tailwind.js'])
+@yield('css')
+@stack('styles')
+@livewireStyles
