9b5b4b21157f6b02dceee5a11e94dcc99edaccbd
tungocvan <tungocvan@gmail.com>
2026-08-22 18:30:16 +0700

    Respect Website service worker appearance setting

--- Modules/Website/resources/views/partials/layout/runtime-scripts.blade.php
+++ Modules/Website/resources/views/partials/layout/runtime-scripts.blade.php
@@ -1,6 +1,8 @@
 @stack('scripts')
+@if(data_get($websiteAppearance ?? [], 'service_worker_enabled', true))
 <script>
     if ('serviceWorker' in navigator) {
         window.addEventListener('load', () => navigator.serviceWorker.register('/service-worker.js').catch(() => {}));
     }
 </script>
+@endif
