e2a345a872522372d4be17e2f0c7913d29e5bcfa
tungocvan <tungocvan@gmail.com>
2026-08-22 10:49:28 +0700

    Split Footer trust badges component

---
+++ Modules/Website/resources/views/components/footer/trust-badges.blade.php
@@ -0,0 +1,6 @@
+<div class="flex items-center gap-4 grayscale opacity-70 hover:grayscale-0 hover:opacity-100 transition-all duration-300">
+    <img src="https://upload.wikimedia.org/wikipedia/commons/5/5e/Visa_Inc._logo.svg" alt="Visa" loading="lazy" class="h-6 w-auto bg-white rounded p-1">
+    <img src="https://upload.wikimedia.org/wikipedia/commons/2/2a/Mastercard-logo.svg" alt="Mastercard" loading="lazy" class="h-6 w-auto bg-white rounded p-1">
+    <img src="https://upload.wikimedia.org/wikipedia/commons/b/b5/PayPal.svg" alt="PayPal" loading="lazy" class="h-6 w-auto bg-white rounded p-1">
+    <img src="https://webmedia.com.vn/images/2021/09/logo-da-thong-bao-bo-cong-thuong-mau-xanh.png" alt="Đã thông báo Bộ Công Thương" loading="lazy" class="h-10 w-auto">
+</div>
