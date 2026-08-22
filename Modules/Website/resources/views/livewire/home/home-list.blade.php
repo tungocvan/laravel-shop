ba5fda80e60cff7ff5d923a69a4107a84e14625d
tungocvan <tungocvan@gmail.com>
2026-08-22 14:29:04 +0700

    Render Homepage sections through registry mapping

--- Modules/Website/resources/views/livewire/home/home-list.blade.php
+++ Modules/Website/resources/views/livewire/home/home-list.blade.php
@@ -3,66 +3,12 @@
         @php
             $visibilityKey = 'show_' . $sectionKey;
             $visibilityClass = $this->getVisibilityClass($visibilityKey);
+            $render = $sectionRenderers[$sectionKey] ?? null;
         @endphp
 
-        @if ($visibilityClass !== 'hidden')
+        @if ($visibilityClass !== 'hidden' && is_array($render) && filled($render['renderer'] ?? null))
             <section class="{{ $visibilityClass }}" wire:key="homepage-section-{{ $sectionKey }}">
-                @php $sectionType = $sectionTypes[$sectionKey] ?? $sectionKey; @endphp
-                @switch($sectionType)
-                    @case('hero')
-                        @livewire('website.home.hero-banner', key('home-'.$sectionKey))
-                    @break
-
-                    @case('categories')
-                    @case('category_grid')
-                        @livewire('website.home.category-highlight', ['categoryIds' => $categoryIds], key('home-'.$sectionKey))
-                    @break
-
-                    @case('flash_sale')
-                        @livewire('website.home.flash-sale', ['lazy' => true], key('home-'.$sectionKey))
-                    @break
-
-                    @case('product_grid')
-                        @if (str_starts_with($sectionKey, 'new_arrivals'))
-                            @livewire('website.home.new-arrivals', ['lazy' => true], key('home-'.$sectionKey))
-                        @elseif (str_starts_with($sectionKey, 'best_sellers'))
-                            @livewire('website.home.best-sellers', ['lazy' => true], key('home-'.$sectionKey))
-                        @else
-                            @livewire('website.home.featured-products', ['lazy' => true], key('home-'.$sectionKey))
-                        @endif
-                    @break
-
-                    @case('featured')
-                        @livewire('website.home.featured-products', ['lazy' => true], key('home-'.$sectionKey))
-                    @break
-
-                    @case('new_arrivals')
-                        @livewire('website.home.new-arrivals', ['lazy' => true], key('home-'.$sectionKey))
-                    @break
-
-                    @case('best_sellers')
-                        @livewire('website.home.best-sellers', ['lazy' => true], key('home-'.$sectionKey))
-                    @break
-
-                    @case('blog_highlight')
-                    @case('post_grid')
-                        @livewire('website.home.blog-highlight', ['lazy' => true], key('home-'.$sectionKey))
-                    @break
-
-                    @case('promo_banner')
-                        @livewire('website.home.promo-banner', ['lazy' => true], key('home-'.$sectionKey))
-                    @break
-
-                    @case('trust_badges')
-                        <div class="hidden md:block">
-                            @livewire('website.home.trust-badges', ['lazy' => true], key('home-'.$sectionKey))
-                        </div>
-                    @break
-
-                    @case('newsletter')
-                        @livewire('website.home.newsletter-signup', ['lazy' => true], key('home-'.$sectionKey))
-                    @break
-                @endswitch
+                @livewire($render['renderer'], $render['params'] ?? [], key('home-'.$sectionKey))
             </section>
         @endif
     @endforeach
