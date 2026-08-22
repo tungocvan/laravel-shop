1dc35ed8f01bd49ae19d67b779ccb63e481bde58
tungocvan <tungocvan@gmail.com>
2026-08-22 18:46:48 +0700

    Add dynamic Website manifest endpoint

--- Modules/Website/Http/Controllers/WebsiteController.php
+++ Modules/Website/Http/Controllers/WebsiteController.php
@@ -1,74 +1,90 @@
 <?php
 
 namespace Modules\Website\Http\Controllers;
+
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Modules\System\Services\SettingsService;
+use Modules\Website\Services\WebsiteAppearanceService;
 
 class WebsiteController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function __construct()
     {
-        //  $this->middleware('permission:website-list|website-create|website-edit|website-delete', ['only' => ['index','show']]);
-        //  $this->middleware('permission:website-create', ['only' => ['create','store']]);
-        //  $this->middleware('permission:website-edit', ['only' => ['edit','update']]);
-        //  $this->middleware('permission:website-delete', ['only' => ['destroy']]);
+        // Reserved for Website-specific middleware.
     }
+
     public function home()
     {
-
         return view('Website::pages.home.index');
     }
+
     public function help()
     {
         return view('Website::pages.help.index');
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
+    public function manifest(SettingsService $settings, WebsiteAppearanceService $appearanceService)
+    {
+        $savedAppearance = $settings->get('website.appearance');
+        $appearance = $appearanceService->resolve(is_array($savedAppearance) ? $savedAppearance : null);
+
+        return response()->json([
+            'name' => $appearance['application_name'],
+            'short_name' => $appearance['apple_title'],
+            'description' => $appearance['application_name'],
+            'lang' => str_replace('_', '-', app()->getLocale()),
+            'start_url' => '/',
+            'scope' => '/',
+            'display' => 'standalone',
+            'orientation' => 'any',
+            'background_color' => $appearance['background_color'],
+            'theme_color' => $appearance['theme_color'],
+            'icons' => [
+                [
+                    'src' => '/pwa/icon.svg',
+                    'sizes' => 'any',
+                    'type' => 'image/svg+xml',
+                    'purpose' => 'any',
+                ],
+                [
+                    'src' => '/pwa/icon-maskable.svg',
+                    'sizes' => 'any',
+                    'type' => 'image/svg+xml',
+                    'purpose' => 'maskable',
+                ],
+            ],
+        ], 200, [
+            'Content-Type' => 'application/manifest+json; charset=UTF-8',
+            'Cache-Control' => 'no-cache, no-store, must-revalidate',
+        ], JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
+    }
+
     public function create()
     {
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
         //
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show(string $id)
     {
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
     public function edit(string $id)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(Request $request, string $id)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(string $id)
     {
         //
