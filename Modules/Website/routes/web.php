1bef6aaf53c09b6053f6ef66536e9b6e0228cfb0
tungocvan <tungocvan@gmail.com>
2026-08-22 18:53:06 +0700

    Route Website PWA version endpoint

--- Modules/Website/routes/web.php
+++ Modules/Website/routes/web.php
@@ -25,6 +25,7 @@ $websitePrefix = config('website.route_prefix', 'website');
 
 Route::get('/sitemap.xml', SitemapController::class)->name('sitemap');
 Route::get('/website-manifest.webmanifest', [WebsiteController::class, 'manifest'])->name('website.manifest');
+Route::get('/website-pwa-version.json', [WebsiteController::class, 'pwaVersion'])->name('website.pwa.version');
 
 Route::middleware('web')->group(function () use ($websitePrefix) {
     Route::controller(AuthController::class)->group(function () {
@@ -42,8 +43,8 @@ Route::middleware('web')->group(function () use ($websitePrefix) {
     });
 
     Route::controller(ProductController::class)->prefix('product')->name('product.')->group(function () {
-        Route::get('/', 'index')->name('list');
-        Route::get('/{slug}', 'show')->name('detail');
+        Route::get('/', 'index')->name('product.list');
+        Route::get('/{slug}', 'show')->name('product.detail');
     });
 
     Route::prefix('blog')->name('blog.')->group(function () {
@@ -59,9 +60,7 @@ Route::middleware('web')->group(function () use ($websitePrefix) {
         Route::get('/', 'index')->name('index');
         Route::get('/success', 'success')->name('success');
         Route::get('/momo-callback', 'momoCallback')->name('momo.callback');
-        Route::post('/momo-ipn', 'momoIpn')
-            ->withoutMiddleware([ValidateCsrfToken::class])
-            ->name('momo.ipn');
+        Route::post('/momo-ipn', 'momoIpn')->withoutMiddleware([ValidateCsrfToken::class])->name('momo.ipn');
     });
 
     Route::middleware('auth')->prefix('account')->name('account.')->controller(AccountController::class)->group(function () {
@@ -74,63 +73,25 @@ Route::middleware('web')->group(function () use ($websitePrefix) {
     });
 });
 
-Route::middleware(['web', 'auth:admin'])
-    ->prefix('admin')
-    ->name('admin.')
-    ->group(function () {
-        Route::get('/website', [WebsiteDashboardController::class, 'index'])
-            ->middleware('permission:website.view,admin')
-            ->name('website.dashboard');
+Route::middleware(['web', 'auth:admin'])->prefix('admin')->name('admin.')->group(function () {
+    Route::get('/website', [WebsiteDashboardController::class, 'index'])->middleware('permission:website.view,admin')->name('website.dashboard');
+    Route::get('/website/settings', [WebsiteSettingsController::class, 'index'])->middleware('permission:website.settings.manage,admin')->name('website.settings');
+    Route::get('/affiliate', [AffiliateController::class, 'index'])->middleware('permission:affiliate.view,admin')->name('affiliate.index');
+    Route::get('/homepage-settings', [HomeSettingsController::class, 'index'])->middleware('permission:website.home.manage,admin')->name('home.settings');
+    Route::get('/header-settings', [HeaderController::class, 'index'])->middleware('permission:website.menu.manage,admin')->name('header.settings');
+    Route::get('/footer-settings', [FooterController::class, 'index'])->middleware('permission:website.footer.manage,admin')->name('footer.settings');
+    Route::get('/banners', [BannerController::class, 'index'])->middleware('permission:website.banner.manage,admin')->name('banners');
+    Route::get('/flash-sales', [FlashSaleController::class, 'index'])->middleware('permission:marketing.flash-sale.view,admin')->name('flash-sales');
 
-        Route::get('/website/settings', [WebsiteSettingsController::class, 'index'])
-            ->middleware('permission:website.settings.manage,admin')
-            ->name('website.settings');
-
-        Route::get('/affiliate', [AffiliateController::class, 'index'])
-            ->middleware('permission:affiliate.view,admin')
-            ->name('affiliate.index');
-
-        Route::get('/homepage-settings', [HomeSettingsController::class, 'index'])
-            ->middleware('permission:website.home.manage,admin')
-            ->name('home.settings');
-
-        Route::get('/header-settings', [HeaderController::class, 'index'])
-            ->middleware('permission:website.menu.manage,admin')
-            ->name('header.settings');
-
-        Route::get('/footer-settings', [FooterController::class, 'index'])
-            ->middleware('permission:website.footer.manage,admin')
-            ->name('footer.settings');
-
-        Route::get('/banners', [BannerController::class, 'index'])
-            ->middleware('permission:website.banner.manage,admin')
-            ->name('banners');
-
-        Route::get('/flash-sales', [FlashSaleController::class, 'index'])
-            ->middleware('permission:marketing.flash-sale.view,admin')
-            ->name('flash-sales');
-
-        Route::prefix('coupons')->name('coupons.')->controller(CouponController::class)->group(function () {
-            Route::get('/', 'index')
-                ->middleware('permission:marketing.coupon.view,admin')
-                ->name('index');
-            Route::get('/create', 'create')
-                ->middleware('permission:marketing.coupon.manage,admin')
-                ->name('create');
-            Route::get('/{id}/edit', 'edit')
-                ->middleware('permission:marketing.coupon.manage,admin')
-                ->name('edit');
-        });
-
-        Route::prefix('customers')->name('customers.')->controller(CustomerController::class)->group(function () {
-            Route::get('/', 'index')
-                ->middleware('permission:customer.view,admin')
-                ->name('index');
-            Route::get('/create', 'create')
-                ->middleware('permission:customer.create,admin')
-                ->name('create');
-            Route::get('/{id}', 'show')
-                ->middleware('permission:customer.view,admin')
-                ->name('show');
-        });
+    Route::prefix('coupons')->name('coupons.')->controller(CouponController::class)->group(function () {
+        Route::get('/', 'index')->middleware('permission:marketing.coupon.view,admin')->name('index');
+        Route::get('/create', 'create')->middleware('permission:marketing.coupon.manage,admin')->name('create');
+        Route::get('/{id}/edit', 'edit')->middleware('permission:marketing.coupon.manage,admin')->name('edit');
     });
+
+    Route::prefix('customers')->name('customers.')->controller(CustomerController::class)->group(function () {
+        Route::get('/', 'index')->middleware('permission:customer.view,admin')->name('index');
+        Route::get('/create', 'create')->middleware('permission:customer.create,admin')->name('create');
+        Route::get('/{id}', 'show')->middleware('permission:customer.view,admin')->name('show');
+    });
+});
