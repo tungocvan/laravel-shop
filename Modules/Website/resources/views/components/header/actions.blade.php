7ec7028475e04cfc8045fd52201b55206faa3c86
tungocvan <tungocvan@gmail.com>
2026-08-22 13:59:55 +0700

    Render Header actions from configurable order

--- Modules/Website/resources/views/components/header/actions.blade.php
+++ Modules/Website/resources/views/components/header/actions.blade.php
@@ -1,3 +1,4 @@
+@php($actions = $headerActions ?? config('website.header.actions', []))
 <div class="flex items-center gap-1 md:gap-6">
     <button type="button" @click="mobileSearchOpen=!mobileSearchOpen" aria-label="Mở tìm kiếm" class="lg:hidden p-2 text-gray-600 hover:bg-gray-100 rounded-full">
         <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"/></svg>
@@ -6,11 +7,14 @@
     @include('Website::components.header.navigation')
 
     <div class="flex items-center gap-2 md:border-l md:border-gray-200 md:pl-6">
-        @livewire('website.wishlist.wishlist-icon')
-        @if(class_exists(\Modules\Website\Livewire\Cart\CartIcon::class))
-            @livewire('website.cart.cart-icon')
-        @endif
-
-        @include('Website::components.header.account')
+        @foreach($actions['order'] ?? ['wishlist','cart','account'] as $action)
+            @if($action === 'wishlist' && ($actions['wishlist']['enabled'] ?? true))
+                @livewire('website.wishlist.wishlist-icon')
+            @elseif($action === 'cart' && ($actions['cart']['enabled'] ?? true) && class_exists(\Modules\Website\Livewire\Cart\CartIcon::class))
+                @livewire('website.cart.cart-icon')
+            @elseif($action === 'account' && ($actions['account']['enabled'] ?? true))
+                @include('Website::components.header.account', ['accountActions' => $actions['account'] ?? []])
+            @endif
+        @endforeach
     </div>
 </div>
