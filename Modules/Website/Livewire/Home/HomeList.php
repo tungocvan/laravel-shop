4d5dc27f2db81a06d14162c31c65b316315f8e3b
tungocvan <tungocvan@gmail.com>
2026-08-22 15:09:10 +0700

    Apply Homepage presentation on frontend

--- Modules/Website/Livewire/Home/HomeList.php
+++ Modules/Website/Livewire/Home/HomeList.php
@@ -3,7 +3,9 @@
 namespace Modules\Website\Livewire\Home;
 
 use Livewire\Component;
+use Modules\System\Services\SettingsService;
 use Modules\Website\Services\HomepageContentService;
+use Modules\Website\Services\HomepagePresentationService;
 use Modules\Website\Services\HomepageSectionRegistry;
 
 class HomeList extends Component
@@ -18,12 +20,25 @@ class HomeList extends Component
 
     public array $sectionRenderers = [];
 
-    public function mount(HomepageContentService $service, HomepageSectionRegistry $registry)
-    {
+    public array $presentation = [];
+
+    public string $homepageContainerClass = '';
+
+    public string $homepageStyle = '';
+
+    public function mount(
+        HomepageContentService $service,
+        HomepageSectionRegistry $registry,
+        SettingsService $settings,
+        HomepagePresentationService $presentationService
+    ) {
         $this->settings = $service->visibility();
         $this->sectionOrder = $service->order();
         $this->sectionTypes = $service->sectionTypes();
         $this->categoryIds = $service->referenceIds('categories', 'category', 'home_category_ids');
+        $this->presentation = $presentationService->resolve($settings->get('homepage.presentation', []));
+        $this->homepageContainerClass = $presentationService->containerClass($this->presentation);
+        $this->homepageStyle = $presentationService->inlineStyle($this->presentation);
 
         foreach ($this->sectionOrder as $sectionKey) {
             try {
