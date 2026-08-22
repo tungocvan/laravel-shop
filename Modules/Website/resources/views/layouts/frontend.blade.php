b15a48a6293ca871ebf455c188b328ffe97cf958
tungocvan <tungocvan@gmail.com>
2026-08-22 17:34:12 +0700

    Add Website shell visibility and maintenance runtime

--- Modules/Website/resources/views/layouts/frontend.blade.php
+++ Modules/Website/resources/views/layouts/frontend.blade.php
@@ -8,17 +8,26 @@
 <body class="bg-website-background text-website-text font-website-body antialiased flex flex-col min-h-screen">
     <a href="#main-content" class="sr-only z-[10000] rounded bg-white px-4 py-2 font-semibold text-blue-700 shadow focus:not-sr-only focus:fixed focus:left-4 focus:top-4">Bỏ qua đến nội dung chính</a>
 
-    @include('Website::partials.header')
+    @if(data_get($websiteShell ?? [], 'header_enabled', true))
+        @include('Website::partials.header')
+    @endif
 
     <main id="main-content" tabindex="-1" class="py-8 w-full flex-grow">
-        @isset($slot)
-            {{ $slot }}
-        @else
-            @yield('content')
-        @endisset
+        @if(data_get($websiteShell ?? [], 'maintenance.enabled', false))
+            @include('Website::partials.layout.maintenance')
+        @elseif(!request()->routeIs('home') || data_get($websiteShell ?? [], 'homepage_enabled', true))
+            @isset($slot)
+                {{ $slot }}
+            @else
+                @yield('content')
+            @endisset
+        @endif
     </main>
 
-    @include('Website::partials.footer')
+    @if(data_get($websiteShell ?? [], 'footer_enabled', true))
+        @include('Website::partials.footer')
+    @endif
+
     @include('Website::partials.layout.global-toast')
     @include('Website::partials.layout.runtime-scripts')
 </body>
