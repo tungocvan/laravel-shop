a3f7c0e7efe0fe7f1a606b4a515d75a41cfeb87e
tungocvan <tungocvan@gmail.com>
2026-08-22 10:49:20 +0700

    Split Footer legal links component

---
+++ Modules/Website/resources/views/components/footer/legal-links.blade.php
@@ -0,0 +1,5 @@
+<div class="flex gap-4 mt-2 justify-center md:justify-start">
+    <a href="#" class="transition-colors hover:text-white">Privacy Policy</a>
+    <a href="#" class="transition-colors hover:text-white">Terms of Service</a>
+    <a href="#" class="transition-colors hover:text-white">Cookie Settings</a>
+</div>
