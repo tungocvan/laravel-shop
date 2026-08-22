014349572c439095192e78c8d61bc1bd8415353c
tungocvan <tungocvan@gmail.com>
2026-08-22 18:10:22 +0700

    Apply Website layout presentation shell class

--- Modules/Website/resources/views/layouts/frontend.blade.php
+++ Modules/Website/resources/views/layouts/frontend.blade.php
@@ -5,14 +5,14 @@
     @include('Website::partials.layout.runtime-head')
 </head>
 
-<body class="bg-website-background text-website-text font-website-body antialiased flex flex-col min-h-screen">
+<body class="text-website-text font-website-body antialiased flex flex-col min-h-screen">
     <a href="#main-content" class="sr-only z-[10000] rounded bg-white px-4 py-2 font-semibold text-blue-700 shadow focus:not-sr-only focus:fixed focus:left-4 focus:top-4">Bỏ qua đến nội dung chính</a>
 
     @if(data_get($websiteShell ?? [], 'header_enabled', true))
         @include('Website::partials.header')
     @endif
 
-    <main id="main-content" tabindex="-1" class="py-8 w-full flex-grow">
+    <main id="main-content" tabindex="-1" class="website-main-shell flex-grow">
         @if(data_get($websiteShell ?? [], 'maintenance.enabled', false))
             @include('Website::partials.layout.maintenance')
         @elseif(!request()->routeIs('home') || data_get($websiteShell ?? [], 'homepage_enabled', true))
