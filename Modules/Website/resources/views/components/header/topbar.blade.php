a19d89e3fecfdeeb8667826f7df78cfa868c2955
tungocvan <tungocvan@gmail.com>
2026-08-22 08:19:58 +0700

    Keep configurable topbar alignment within selected container

--- Modules/Website/resources/views/components/header/topbar.blade.php
+++ Modules/Website/resources/views/components/header/topbar.blade.php
@@ -1,5 +1,5 @@
-<div class="text-xs hidden md:block" style="min-height: var(--header-topbar-height); background: {{ ($headerPresentation['inherit_colors'] ?? true) ? 'var(--website-color-text)' : ($headerPresentation['colors']['topbar_background'] ?? '#111827') }}; color: {{ ($headerPresentation['inherit_colors'] ?? true) ? 'var(--website-color-surface)' : ($headerPresentation['colors']['topbar_foreground'] ?? '#ffffff') }};">
-    <div class="mx-auto px-4 flex min-h-[inherit] justify-between items-center" @if(!empty($headerPresentation['container_width'])) style="max-width: {{ (int)$headerPresentation['container_width'] }}px" @endif>
+<div class="text-xs hidden md:block" style="background: {{ ($headerPresentation['inherit_colors'] ?? true) ? 'var(--website-color-text)' : ($headerPresentation['colors']['topbar_background'] ?? '#111827') }}; color: {{ ($headerPresentation['inherit_colors'] ?? true) ? 'var(--website-color-surface)' : ($headerPresentation['colors']['topbar_foreground'] ?? '#ffffff') }};">
+    <div class="mx-auto px-4 flex justify-between items-center" style="min-height: var(--header-topbar-height); {{ !empty($headerPresentation['container_width']) ? 'max-width: '.(int)$headerPresentation['container_width'].'px;' : '' }}">
         <div class="flex items-center gap-4">
             <a href="tel:{{ preg_replace('/[^0-9+]/', '', $headerSettings['hotline'] ?? '') }}" class="hover:opacity-80">{{ $headerSettings['hotline'] ?? '1900 xxxx' }}</a>
             <span class="opacity-40">|</span>
