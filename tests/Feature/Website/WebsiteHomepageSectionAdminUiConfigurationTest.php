c8bfd6222175ae3035256da6f5cfc3784adb375c
tungocvan <tungocvan@gmail.com>
2026-08-22 14:54:47 +0700

    Fix Homepage 11C test string interpolation

--- tests/Feature/Website/WebsiteHomepageSectionAdminUiConfigurationTest.php
+++ tests/Feature/Website/WebsiteHomepageSectionAdminUiConfigurationTest.php
@@ -13,14 +13,14 @@ class WebsiteHomepageSectionAdminUiConfigurationTest extends TestCase
         $view = file_get_contents(base_path('Modules/Website/resources/views/livewire/admin/home/home-settings-v2.blade.php'));
         $legacyView = file_get_contents(base_path('Modules/Website/resources/views/livewire/admin/home/home-settings.blade.php'));
 
-        $this->assertStringContainsString("'sectionCards' => $registry->adminCards($this->sectionOrder, $this->sectionTypes)", $component);
+        $this->assertStringContainsString("'sectionCards' => \$registry->adminCards(\$this->sectionOrder, \$this->sectionTypes)", $component);
         $this->assertStringContainsString('public function adminCards(array $sectionOrder, array $sectionTypes = [])', $registry);
-        $this->assertStringContainsString("'admin' => $this->adminAction($sectionKey)", $registry);
+        $this->assertStringContainsString("'admin' => \$this->adminAction(\$sectionKey)", $registry);
 
         $this->assertStringContainsString('@foreach($sectionCards as $card)', $view);
-        $this->assertStringContainsString("$card['admin']['type'] === 'route'", $view);
-        $this->assertStringContainsString("wire:click=\"setTab('{{ $card['admin']['tab'] }}')\"", $view);
-        $this->assertStringContainsString("{{ $card['admin']['label'] }}", $view);
+        $this->assertStringContainsString("\$card['admin']['type'] === 'route'", $view);
+        $this->assertStringContainsString("wire:click=\"setTab('{{ \$card['admin']['tab'] }}')\"", $view);
+        $this->assertStringContainsString("{{ \$card['admin']['label'] }}", $view);
         $this->assertStringNotContainsString('$sections = [', $view);
 
         $this->assertStringContainsString('border border-gray-300 bg-white px-3 py-2.5', $view);
