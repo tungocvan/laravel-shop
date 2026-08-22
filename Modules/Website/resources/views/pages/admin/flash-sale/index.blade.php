e32e49db0ed33d45c47cdbe665e824efc42264e9
tungocvan <tungocvan@gmail.com>
2026-08-22 15:52:54 +0700

    Add Homepage return link to Flash Sale manager

--- Modules/Website/resources/views/pages/admin/flash-sale/index.blade.php
+++ Modules/Website/resources/views/pages/admin/flash-sale/index.blade.php
@@ -1,4 +1,14 @@
 @extends('Admin::layouts.master')
+
 @section('content')
+    @if(request('from') === 'homepage')
+        <div class="mx-auto mb-4 max-w-7xl">
+            <a href="{{ route('admin.home.settings') }}"
+                class="inline-flex items-center rounded-lg border border-gray-300 bg-white px-3 py-2 text-sm font-semibold text-gray-700 shadow-sm hover:bg-gray-50">
+                ← Quay lại bố cục
+            </a>
+        </div>
+    @endif
+
     @livewire('website.admin.flash-sale.flash-sale-manager')
 @endsection
