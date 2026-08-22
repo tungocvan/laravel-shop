91ea4715330d85caf924b4d99bee30ebbec4b958
tungocvan <tungocvan@gmail.com>
2026-08-22 15:43:44 +0700

    Test focused Homepage admin workspaces

--- tests/Feature/Website/WebsiteHomepageSectionAdminUiConfigurationTest.php
+++ tests/Feature/Website/WebsiteHomepageSectionAdminUiConfigurationTest.php
@@ -10,28 +10,27 @@ class WebsiteHomepageSectionAdminUiConfigurationTest extends TestCase
     {
         $component = file_get_contents(base_path('Modules/Website/Livewire/Admin/Home/HomeSettings.php'));
         $registry = file_get_contents(base_path('Modules/Website/Services/HomepageSectionRegistry.php'));
-        $shellView = file_get_contents(base_path('Modules/Website/resources/views/livewire/admin/home/home-settings-v3.blade.php'));
-        $builderView = file_get_contents(base_path('Modules/Website/resources/views/livewire/admin/home/home-settings-v2.blade.php'));
-        $view = $shellView."\n".$builderView;
-        $legacyView = file_get_contents(base_path('Modules/Website/resources/views/livewire/admin/home/home-settings.blade.php'));
+        $shell = file_get_contents(base_path('Modules/Website/resources/views/livewire/admin/home/home-settings-v3.blade.php'));
+        $builder = file_get_contents(base_path('Modules/Website/resources/views/livewire/admin/home/partials/layout-builder.blade.php'));
+        $editor = file_get_contents(base_path('Modules/Website/resources/views/livewire/admin/home/partials/section-editor.blade.php'));
+        $view = $shell."\n".$builder."\n".$editor;
 
         $this->assertStringContainsString("'sectionCards' => \$registry->adminCards(\$this->sectionOrder, \$this->sectionTypes)", $component);
         $this->assertStringContainsString('public function adminCards(array $sectionOrder, array $sectionTypes = [])', $registry);
         $this->assertStringContainsString("'admin' => \$this->adminAction(\$sectionKey)", $registry);
 
-        $this->assertStringContainsString("@include('Website::livewire.admin.home.home-settings-v2')", $shellView);
-        $this->assertStringContainsString('@foreach($sectionCards as $card)', $view);
-        $this->assertStringContainsString("\$card['admin']['type'] === 'route'", $view);
-        $this->assertStringContainsString("wire:click=\"setTab('{{ \$card['admin']['tab'] }}')\"", $view);
-        $this->assertStringContainsString("{{ \$card['admin']['label'] }}", $view);
+        $this->assertStringContainsString("@include('Website::livewire.admin.home.partials.layout-builder')", $shell);
+        $this->assertStringContainsString("@include('Website::livewire.admin.home.partials.section-editor')", $shell);
+        $this->assertStringContainsString('@foreach($sectionCards as $card)', $builder);
+        $this->assertStringContainsString("\$card['admin']['type'] === 'route'", $builder);
+        $this->assertStringContainsString("\$set('activeTab', '{{ \$card['admin']['tab'] }}')", $builder);
+        $this->assertStringContainsString("{{ \$card['admin']['label'] }}", $builder);
         $this->assertStringNotContainsString('$sections = [', $view);
 
         $this->assertStringContainsString('border border-gray-300 bg-white px-3 py-2.5', $view);
         $this->assertStringContainsString('focus:ring-2 focus:ring-indigo-100', $view);
-        $this->assertStringContainsString('sticky bottom-4', $view);
-        $this->assertStringContainsString('border border-dashed border-gray-300', $view);
-
+        $this->assertStringContainsString('sticky bottom-4', $shell);
+        $this->assertStringContainsString('border border-dashed border-gray-300', $editor);
         $this->assertStringContainsString('home-settings-v3', $component);
-        $this->assertStringContainsString('$sections = [', $legacyView);
     }
 }
