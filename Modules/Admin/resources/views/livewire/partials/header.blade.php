060face41d583e98d917323b4c532117efb8ff70
tungocvan <tungocvan@gmail.com>
2026-08-23 11:17:40 +0700

    fix(admin): pass component data into header partials

--- Modules/Admin/resources/views/livewire/partials/header.blade.php
+++ Modules/Admin/resources/views/livewire/partials/header.blade.php
@@ -9,13 +9,13 @@
     <div class="flex min-w-0 flex-1 items-center justify-between gap-3 px-4 sm:px-6 lg:px-8">
         <div class="flex min-w-0 flex-1 items-center gap-2 sm:gap-3">
             @foreach (($headerContext['left'] ?? []) as $component)
-                @include($component['view'])
+                @include($component['view'], $component['data'] ?? [])
             @endforeach
         </div>
 
         <div class="flex shrink-0 items-center gap-1.5 sm:gap-2">
             @foreach (($headerContext['right'] ?? []) as $component)
-                @include($component['view'])
+                @include($component['view'], $component['data'] ?? [])
             @endforeach
         </div>
     </div>
