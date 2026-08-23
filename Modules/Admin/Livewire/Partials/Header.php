b1cc788f0e6c29f70706237b5c2c6f472dda4695
tungocvan <tungocvan@gmail.com>
2026-08-23 07:14:22 +0700

    refactor(admin): provide header composition context

--- Modules/Admin/Livewire/Partials/Header.php
+++ Modules/Admin/Livewire/Partials/Header.php
@@ -3,9 +3,17 @@
 namespace Modules\Admin\Livewire\Partials;
 
 use Livewire\Component;
+use Modules\Admin\Services\AdminHeaderService;
 
 class Header extends Component
 {
+    public array $headerContext = [];
+
+    public function mount(AdminHeaderService $headerService): void
+    {
+        $this->headerContext = $headerService->context();
+    }
+
     public function render()
     {
         return view('Admin::livewire.partials.header');
