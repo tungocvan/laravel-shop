8300bee67affb83baa2d831884d7d5aac015dd56
tungocvan <tungocvan@gmail.com>
2026-08-23 07:58:32 +0700

    feat(admin): apply container and density presentation

--- Modules/Admin/resources/views/layouts/partials/content.blade.php
+++ Modules/Admin/resources/views/layouts/partials/content.blade.php
@@ -1,5 +1,5 @@
 <main id="admin-main" tabindex="-1" class="min-h-0 flex-1 overflow-y-auto focus:outline-none">
-    <div class="w-full px-4 py-5 sm:px-5 lg:px-6 lg:py-6">
+    <div class="w-full {{ $adminShellPresentation['content_class'] }} {{ $adminShellPresentation['content_padding_class'] }}">
         @include('Admin::layouts.partials.flash')
 
         @isset($slot)
