cd45da17c1d5c399e3c14f5668452c6d39ccce51
tungocvan <tungocvan@gmail.com>
2026-08-23 12:25:15 +0700

    test(admin): lock surfaced header action icon treatment

--- tests/Feature/Admin/AdminHeaderActionsContractTest.php
+++ tests/Feature/Admin/AdminHeaderActionsContractTest.php
@@ -43,6 +43,23 @@ class AdminHeaderActionsContractTest extends TestCase
         $this->assertStringContainsString('rel="noopener noreferrer"', $view);
     }
 
+    public function test_header_action_icons_use_visible_accent_surfaces(): void
+    {
+        $actions = file_get_contents(base_path('Modules/Admin/resources/views/livewire/partials/header/components/actions.blade.php'));
+        $notifications = file_get_contents(base_path('Modules/Admin/resources/views/livewire/partials/header-notifications.blade.php'));
+
+        foreach ([$actions, $notifications] as $view) {
+            $this->assertStringContainsString('border-indigo-100', $view);
+            $this->assertStringContainsString('bg-indigo-50', $view);
+            $this->assertStringContainsString('text-indigo-600', $view);
+            $this->assertStringContainsString('hover:bg-indigo-100', $view);
+        }
+
+        $this->assertStringContainsString('$actionButtonClass', $actions);
+        $this->assertStringContainsString('hover:shadow-md', $actions);
+        $this->assertStringContainsString('ring-2 ring-white', $actions);
+    }
+
     public function test_secondary_actions_use_mobile_overflow_without_hiding_primary_actions(): void
     {
         $view = file_get_contents(base_path('Modules/Admin/resources/views/livewire/partials/header/components/actions.blade.php'));
