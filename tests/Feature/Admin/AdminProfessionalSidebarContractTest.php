4143d63d774cf4a25bd3e6feaeca005451a587aa
tungocvan <tungocvan@gmail.com>
2026-08-23 08:29:05 +0700

    test(admin): cover configurable sidebar search threshold

--- tests/Feature/Admin/AdminProfessionalSidebarContractTest.php
+++ tests/Feature/Admin/AdminProfessionalSidebarContractTest.php
@@ -11,10 +11,14 @@ class AdminProfessionalSidebarContractTest extends TestCase
         $component = file_get_contents(base_path('Modules/Admin/Livewire/Partials/Sidebar.php'));
         $view = file_get_contents(base_path('Modules/Admin/resources/views/livewire/partials/sidebar.blade.php'));
         $group = file_get_contents(base_path('Modules/Admin/resources/views/livewire/partials/sidebar/navigation/group.blade.php'));
+        $manager = file_get_contents(base_path('Modules/Admin/Support/AdminLayoutManager.php'));
+        $settings = file_get_contents(base_path('Modules/Admin/Livewire/Settings/AdminLayoutConfig.php'));
 
         $this->assertStringContainsString('public int $destinationCount = 0;', $component);
-        $this->assertStringContainsString('public bool $showNavigationSearch = false;', $component);
-        $this->assertStringContainsString('$this->destinationCount >= 12', $component);
+        $this->assertStringContainsString('sidebar.navigation_search_threshold', $component);
+        $this->assertStringContainsString('$this->destinationCount >= $searchThreshold', $component);
+        $this->assertStringContainsString('navigation_search_threshold', $manager);
+        $this->assertStringContainsString('config.sidebar.navigation_search_threshold', $settings);
         $this->assertStringContainsString('@if ($showNavigationSearch)', $view);
         $this->assertStringContainsString('Tìm chức năng...', $view);
         $this->assertStringContainsString('x-model.debounce.120ms="navQuery"', $view);
@@ -28,7 +32,6 @@ class AdminProfessionalSidebarContractTest extends TestCase
     public function test_sidebar_has_clear_workspace_and_profile_hierarchy(): void
     {
         $view = file_get_contents(base_path('Modules/Admin/resources/views/livewire/partials/sidebar.blade.php'));
-
         $this->assertStringContainsString('Không gian quản trị', $view);
         $this->assertStringContainsString('Điều hướng', $view);
         $this->assertStringContainsString('Tài khoản quản trị', $view);
@@ -39,7 +42,6 @@ class AdminProfessionalSidebarContractTest extends TestCase
     {
         $item = file_get_contents(base_path('Modules/Admin/resources/views/livewire/partials/sidebar/navigation/item.blade.php'));
         $group = file_get_contents(base_path('Modules/Admin/resources/views/livewire/partials/sidebar/navigation/group.blade.php'));
-
         $this->assertStringContainsString("href=\"{{ \$item['href'] }}\"", $item);
         $this->assertStringContainsString('aria-current="page"', $item);
         $this->assertStringContainsString('bg-indigo-50 text-indigo-700', $item);
@@ -53,7 +55,6 @@ class AdminProfessionalSidebarContractTest extends TestCase
         $sidebar = file_get_contents(base_path('Modules/Admin/resources/views/livewire/partials/sidebar.blade.php'));
         $item = file_get_contents(base_path('Modules/Admin/resources/views/livewire/partials/sidebar/navigation/item.blade.php'));
         $group = file_get_contents(base_path('Modules/Admin/resources/views/livewire/partials/sidebar/navigation/group.blade.php'));
-
         foreach ([$sidebar, $item, $group] as $view) {
             $this->assertStringNotContainsString('auth()->user()', $view);
             $this->assertStringNotContainsString('->can(', $view);
