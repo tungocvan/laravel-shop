9631cdae36a50522862b23f4756f47d550a1bbf5
tungocvan <tungocvan@gmail.com>
2026-08-23 08:26:55 +0700

    test(admin): assert high-volume filter gating

--- tests/Feature/Admin/AdminProfessionalSidebarContractTest.php
+++ tests/Feature/Admin/AdminProfessionalSidebarContractTest.php
@@ -20,7 +20,7 @@ class AdminProfessionalSidebarContractTest extends TestCase
         $this->assertStringContainsString('x-model.debounce.120ms="navQuery"', $view);
         $this->assertStringContainsString("collect(\$item['children'] ?? [])->pluck('name')", $view);
         $this->assertStringContainsString('x-show="matches(@js(', $view);
-        $this->assertStringContainsString("open || navQuery.trim() !== ''", $group);
+        $this->assertStringContainsString("\$showNavigationSearch ? 'true' : 'false'", $group);
         $this->assertStringContainsString("matches(@js(\$child['name']))", $group);
         $this->assertStringContainsString('[scrollbar-gutter:stable]', $view);
     }
