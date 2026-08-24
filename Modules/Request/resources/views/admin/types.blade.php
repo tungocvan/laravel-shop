20138ba500b1d4a73c56b0177cf1ff1d2b8c90f2
tungocvan <tungocvan@gmail.com>
2026-08-24 19:47:15 +0700

    feat(request): link type manager back to dashboard

--- Modules/Request/resources/views/admin/types.blade.php
+++ Modules/Request/resources/views/admin/types.blade.php
@@ -1,5 +1,10 @@
 @extends('Admin::layouts.master')
 @section('title', __('Request::request.types.title'))
 @section('content')
-<div class="p-4 sm:p-6"><h1 class="text-2xl font-semibold">{{ __('Request::request.types.title') }}</h1><p class="mt-1 text-sm text-gray-600">{{ __('Request::request.types.description') }}</p><div class="mt-6">@livewire('request.admin.definition-index')</div></div>
+<div class="p-4 sm:p-6">
+    @include('Request::partials.dashboard-back')
+    <h1 class="text-2xl font-semibold">{{ __('Request::request.types.title') }}</h1>
+    <p class="mt-1 text-sm text-gray-600">{{ __('Request::request.types.description') }}</p>
+    <div class="mt-6">@livewire('request.admin.definition-index')</div>
+</div>
 @endsection
