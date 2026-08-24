b2f92b37e0242bc2a70ccb4453902798f09463f2
tungocvan <tungocvan@gmail.com>
2026-08-24 16:24:21 +0700

    feat(request): enable safe offline runtime on my requests

--- Modules/Request/resources/views/requester/mine.blade.php
+++ Modules/Request/resources/views/requester/mine.blade.php
@@ -1,5 +1,8 @@
 @extends('Admin::layouts.master')
 @section('title', __('Request::request.mine.title'))
 @section('content')
-<div class="p-4 sm:p-6">@livewire('request.requester.my-requests')</div>
+<div class="p-4 sm:p-6">
+    @include('Request::partials.offline-runtime')
+    @livewire('request.requester.my-requests')
+</div>
 @endsection
