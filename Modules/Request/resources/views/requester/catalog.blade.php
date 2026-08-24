adac8fb7ac14a78035d6cdfb6dcce25384f58785
tungocvan <tungocvan@gmail.com>
2026-08-24 16:24:07 +0700

    feat(request): enable safe offline runtime on catalog

--- Modules/Request/resources/views/requester/catalog.blade.php
+++ Modules/Request/resources/views/requester/catalog.blade.php
@@ -1,5 +1,8 @@
 @extends('Admin::layouts.master')
 @section('title', __('Request::request.catalog.title'))
 @section('content')
-<div class="p-4 sm:p-6">@livewire('request.requester.catalog')</div>
+<div class="p-4 sm:p-6">
+    @include('Request::partials.offline-runtime')
+    @livewire('request.requester.catalog')
+</div>
 @endsection
