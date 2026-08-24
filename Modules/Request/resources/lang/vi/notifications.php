634b8274b321b6e553ad1013caff0b1371ba6d9f
tungocvan <tungocvan@gmail.com>
2026-08-24 15:55:00 +0700

    feat(request): add MR-07 Vietnamese notification copy

---
+++ Modules/Request/resources/lang/vi/notifications.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'subject' => 'Cập nhật đề nghị :number',
+    'greeting' => 'Thông báo đề nghị',
+    'summary' => ':title (:number) có cập nhật mới.',
+    'status' => 'Trạng thái hiện tại: :status',
+    'open' => 'Mở đề nghị',
+];
