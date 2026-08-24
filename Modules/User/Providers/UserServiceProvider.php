337de88bc21a1a37478ebb07867c1480fad5a8a7
tungocvan <tungocvan@gmail.com>
2026-08-24 15:54:13 +0700

    feat(request): bind MR-07 notification shell adapter

--- Modules/User/Providers/UserServiceProvider.php
+++ Modules/User/Providers/UserServiceProvider.php
@@ -5,8 +5,10 @@ namespace Modules\User\Providers;
 use Illuminate\Support\ServiceProvider;
 use Modules\User\Contracts\UserDirectory;
 use Modules\User\Contracts\UserMailGateway;
+use Modules\User\Contracts\UserNotifier;
 use Modules\User\Services\AuthUserDirectory;
 use Modules\User\Services\AuthUserMailGateway;
+use Modules\User\Services\AuthUserNotifier;
 
 class UserServiceProvider extends ServiceProvider
 {
@@ -14,5 +16,6 @@ class UserServiceProvider extends ServiceProvider
     {
         $this->app->singleton(UserDirectory::class, AuthUserDirectory::class);
         $this->app->singleton(UserMailGateway::class, AuthUserMailGateway::class);
+        $this->app->singleton(UserNotifier::class, AuthUserNotifier::class);
     }
 }
