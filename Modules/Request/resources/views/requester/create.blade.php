6a59bd386ab5843a1a48f50beb93ca95a3e8cd20
tungocvan <tungocvan@gmail.com>
2026-08-24 16:24:14 +0700

    feat(request): enable safe offline runtime on draft create

--- Modules/Request/resources/views/requester/create.blade.php
+++ Modules/Request/resources/views/requester/create.blade.php
@@ -1,5 +1,8 @@
 @extends('Admin::layouts.master')
 @section('title', __('Request::request.create_draft'))
 @section('content')
-<div class="p-4 sm:p-6">@livewire('request.requester.create-draft', ['typePublicId' => $typePublicId])</div>
+<div class="p-4 sm:p-6">
+    @include('Request::partials.offline-runtime')
+    @livewire('request.requester.create-draft', ['typePublicId' => $typePublicId])
+</div>
 @endsection
