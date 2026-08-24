3857e83b663fe0e97584c872d2b0d856b7a15000
tungocvan <tungocvan@gmail.com>
2026-08-24 19:44:52 +0700

    feat(request): link my requests back to dashboard

--- Modules/Request/resources/views/requester/mine.blade.php
+++ Modules/Request/resources/views/requester/mine.blade.php
@@ -3,6 +3,7 @@
 @section('content')
 <div class="p-4 sm:p-6">
     @include('Request::partials.offline-runtime')
+    @include('Request::partials.dashboard-back')
     @livewire('request.requester.my-requests')
 </div>
 @endsection
