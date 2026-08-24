9d0c4c7d8cb0aa57be0fed45e0d60e9afe594526
tungocvan <tungocvan@gmail.com>
2026-08-24 19:56:55 +0700

    fix(request): apply Vietnamese locale to web routes

--- Modules/Request/routes/web.php
+++ Modules/Request/routes/web.php
@@ -5,15 +5,16 @@ use Modules\Request\Http\Controllers\RequestAttachmentController;
 use Modules\Request\Http\Controllers\RequestDashboardController;
 use Modules\Request\Http\Controllers\RequestDefinitionController;
 use Modules\Request\Http\Controllers\RequestRequesterController;
+use Modules\Request\Http\Middleware\UseVietnameseRequestLocale;
 
-Route::middleware(['web', 'auth:admin'])->prefix('admin/requests/admin')->name('request.admin.')->group(function (): void {
+Route::middleware(['web', UseVietnameseRequestLocale::class, 'auth:admin'])->prefix('admin/requests/admin')->name('request.admin.')->group(function (): void {
     Route::get('/groups', [RequestDefinitionController::class, 'groups'])->middleware('permission:request.group.view,admin')->name('groups');
     Route::get('/types', [RequestDefinitionController::class, 'types'])->middleware('permission:request.type.view,admin')->name('types');
     Route::get('/types/{typePublicId}/designer', [RequestDefinitionController::class, 'designer'])->whereUlid('typePublicId')->middleware('permission:request.type.update,admin')->name('types.designer');
     Route::get('/types/{typePublicId}/versions', [RequestDefinitionController::class, 'versions'])->whereUlid('typePublicId')->middleware('permission:request.type.view,admin')->name('types.versions');
 });
 
-Route::middleware(['web', 'auth:admin'])->prefix('admin/requests')->name('request.')->group(function (): void {
+Route::middleware(['web', UseVietnameseRequestLocale::class, 'auth:admin'])->prefix('admin/requests')->name('request.')->group(function (): void {
     Route::get('/', RequestDashboardController::class)->middleware('permission:request.dashboard.view,admin')->name('dashboard');
     Route::get('/catalog', [RequestRequesterController::class, 'catalog'])->middleware('permission:request.instance.create,admin')->name('catalog');
     Route::get('/create/{typePublicId}', [RequestRequesterController::class, 'create'])->whereUlid('typePublicId')->middleware('permission:request.instance.create,admin')->name('create');
