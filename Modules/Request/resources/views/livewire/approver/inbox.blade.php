d0fd2f1a3492eb5d6c56e0db547010f3289215a4
tungocvan <tungocvan@gmail.com>
2026-08-25 19:45:54 +0700

    ux(request): mount workspace navigation on approval inbox

--- Modules/Request/resources/views/livewire/approver/inbox.blade.php
+++ Modules/Request/resources/views/livewire/approver/inbox.blade.php
@@ -1,4 +1,6 @@
 <div class="mx-auto max-w-6xl space-y-5">
+    @include('Request::partials.workspace-navigation')
+
     <header>
         <h1 class="text-2xl font-bold text-gray-900">{{ __('Request::request.inbox.title') }}</h1>
         <p class="mt-1 text-sm text-gray-600">{{ __('Request::request.inbox.subtitle') }}</p>
