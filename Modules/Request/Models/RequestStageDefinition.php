7bdf38cb337e17620b0256bbbafb3a36c8d2d1d7
tungocvan <tungocvan@gmail.com>
2026-08-25 10:52:01 +0700

    feat(request): expose stage SLA settings

--- Modules/Request/Models/RequestStageDefinition.php
+++ Modules/Request/Models/RequestStageDefinition.php
@@ -18,11 +18,33 @@ class RequestStageDefinition extends Model
         return RequestStageDefinitionFactory::new();
     }
 
-    protected $fillable = ['request_type_version_id', 'stage_key', 'name', 'position', 'mode', 'resolver_key', 'resolver_config_json', 'instructions', 'allow_reassignment'];
+    protected $fillable = [
+        'request_type_version_id',
+        'stage_key',
+        'name',
+        'position',
+        'mode',
+        'resolver_key',
+        'resolver_config_json',
+        'instructions',
+        'allow_reassignment',
+        'sla_minutes',
+        'warning_minutes_before',
+        'grace_minutes',
+        'timeout_action',
+    ];
 
     protected function casts(): array
     {
-        return ['position' => 'integer', 'mode' => StageMode::class, 'resolver_config_json' => 'array', 'allow_reassignment' => 'boolean'];
+        return [
+            'position' => 'integer',
+            'mode' => StageMode::class,
+            'resolver_config_json' => 'array',
+            'allow_reassignment' => 'boolean',
+            'sla_minutes' => 'integer',
+            'warning_minutes_before' => 'integer',
+            'grace_minutes' => 'integer',
+        ];
     }
 
     public function version(): BelongsTo
