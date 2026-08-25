725e62293f79eb34fbb1e09f3a7744fae7dfb06d
tungocvan <tungocvan@gmail.com>
2026-08-26 05:22:02 +0700

    feat(request): add version history release actor context

--- Modules/Request/Http/Controllers/RequestDefinitionController.php
+++ Modules/Request/Http/Controllers/RequestDefinitionController.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Gate;
 use Illuminate\View\View;
 use Modules\Request\Models\RequestGroup;
 use Modules\Request\Models\RequestType;
+use Modules\User\Contracts\UserDirectory;
 
 final class RequestDefinitionController extends Controller
 {
@@ -32,15 +33,26 @@ final class RequestDefinitionController extends Controller
         return view('Request::admin.designer', compact('type'));
     }
 
-    public function versions(string $typePublicId): View
+    public function versions(string $typePublicId, UserDirectory $users): View
     {
         $type = RequestType::query()->where('public_id', $typePublicId)->with([
+            'activeDraft:id,request_type_id,version_number',
+            'currentPublishedVersion:id,request_type_id,version_number',
             'versions' => fn ($query) => $query->latest('version_number'),
             'versions.audiences',
             'versions.stages',
         ])->firstOrFail();
         Gate::authorize('view', $type);
 
-        return view('Request::admin.versions', compact('type'));
+        $actorIds = $type->versions
+            ->flatMap(fn ($version) => [$version->created_by, $version->published_by])
+            ->filter()
+            ->unique()
+            ->values()
+            ->all();
+
+        $versionActors = collect($users->findManyActive($actorIds, 100))->keyBy('id');
+
+        return view('Request::admin.versions', compact('type', 'versionActors'));
     }
 }
