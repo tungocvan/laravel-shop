cef9f8b70fc61952da728ed6944b6fa70e61778a
tungocvan <tungocvan@gmail.com>
2026-08-26 16:55:01 +0700

    fix(client-portal): load Livewire assets for request create

--- Modules/ClientPortal/resources/views/applications/request/create.blade.php
+++ Modules/ClientPortal/resources/views/applications/request/create.blade.php
@@ -6,5 +6,7 @@
 @section('mobile-nav')@include('ClientPortal::applications.request.partials.mobile-nav')@endsection
 
 @section('content')
+    @livewireStyles
     @livewire('request.requester.create-draft', ['typePublicId' => $typePublicId])
+    @livewireScripts
 @endsection
