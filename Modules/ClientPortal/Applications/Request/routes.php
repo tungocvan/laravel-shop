e891314f0e1e0c0b37c06044873c34486201825a
tungocvan <tungocvan@gmail.com>
2026-08-26 20:23:32 +0700

    feat(clientportal): add request approver routes

--- Modules/ClientPortal/Applications/Request/routes.php
+++ Modules/ClientPortal/Applications/Request/routes.php
@@ -32,5 +32,12 @@ if ((bool) config('modules.registry.Request.enabled', false)) {
         Route::get('/mine/{requestPublicId}/attachments/{attachmentPublicId}', RequestAttachmentController::class)
             ->middleware(['client.feature:request,mine', 'throttle:request-download'])
             ->name('attachments.download');
+
+        Route::get('/inbox', [RequestApplicationController::class, 'inbox'])
+            ->middleware('client.feature:request,inbox')
+            ->name('inbox');
+        Route::get('/processed', [RequestApplicationController::class, 'processed'])
+            ->middleware('client.feature:request,processed')
+            ->name('processed');
     });
 }
