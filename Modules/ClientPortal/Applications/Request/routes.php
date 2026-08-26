29b0cb2603e90e0101db1fb636bbf4075ad979fb
tungocvan <tungocvan@gmail.com>
2026-08-26 16:49:42 +0700

    feat(client-portal): expose requester request routes

--- Modules/ClientPortal/Applications/Request/routes.php
+++ Modules/ClientPortal/Applications/Request/routes.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use Modules\ClientPortal\Applications\Request\Http\Controllers\RequestApplicationController;
+use Modules\Request\Http\Controllers\RequestAttachmentController;
 use Modules\Request\Http\Middleware\UseRequestAuthorizationGuard;
 
 if ((bool) config('modules.registry.Request.enabled', false)) {
@@ -14,5 +15,22 @@ if ((bool) config('modules.registry.Request.enabled', false)) {
         Route::get('/', [RequestApplicationController::class, 'dashboard'])
             ->middleware('client.feature:request,overview')
             ->name('dashboard');
+
+        Route::get('/catalog', [RequestApplicationController::class, 'catalog'])
+            ->middleware('client.feature:request,create')
+            ->name('catalog');
+        Route::get('/catalog/{typePublicId}', [RequestApplicationController::class, 'create'])
+            ->middleware('client.feature:request,create')
+            ->name('create');
+
+        Route::get('/mine', [RequestApplicationController::class, 'mine'])
+            ->middleware('client.feature:request,mine')
+            ->name('mine');
+        Route::get('/mine/{requestPublicId}', [RequestApplicationController::class, 'show'])
+            ->middleware('client.feature:request,mine')
+            ->name('show');
+        Route::get('/mine/{requestPublicId}/attachments/{attachmentPublicId}', RequestAttachmentController::class)
+            ->middleware(['client.feature:request,mine', 'throttle:request-download'])
+            ->name('attachments.download');
     });
 }
