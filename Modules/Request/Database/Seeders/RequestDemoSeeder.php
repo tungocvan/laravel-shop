fa9af6ada2d7d85c2367c84be8d48a6359c0d891
tungocvan <tungocvan@gmail.com>
2026-08-27 16:22:01 +0700

    chore(request): report skipped demo seeding

--- Modules/Request/Database/Seeders/RequestDemoSeeder.php
+++ Modules/Request/Database/Seeders/RequestDemoSeeder.php
@@ -9,8 +9,12 @@ class RequestDemoSeeder extends Seeder
     public function run(): void
     {
         if (config('request.settings.demo_seeders_enabled', false) !== true) {
+            $this->command?->warn('RequestDemoSeeder skipped: demo seeders are disabled for this environment.');
+
             return;
         }
+
+        $this->command?->info('RequestDemoSeeder enabled: seeding Request starter templates.');
 
         $this->call([
             RequestStarterTemplateSeeder::class,
