d999bd0293420fe180375b50d8dfd0f2f7897d24
tungocvan <tungocvan@gmail.com>
2026-08-30 13:44:23 +0700

    ui(product): use white inactive pagination controls

--- Modules/Product/resources/views/pages/products/index.blade.php
+++ Modules/Product/resources/views/pages/products/index.blade.php
@@ -2,15 +2,11 @@
 
 @section('content')
     <style>
-        .product-admin-workspace nav[role="navigation"] [aria-current="page"] > span {
-            background-color: rgb(79 70 229) !important;
-            border-color: rgb(79 70 229) !important;
-            color: white !important;
-        }
-
-        .product-admin-workspace nav[role="navigation"] a {
+        .product-admin-workspace nav[role="navigation"] a,
+        .product-admin-workspace nav[role="navigation"] span {
             color: rgb(75 85 99) !important;
             background-color: white !important;
+            border-color: rgb(229 231 235) !important;
         }
 
         .product-admin-workspace nav[role="navigation"] a:hover {
@@ -19,9 +15,15 @@
             border-color: rgb(199 210 254) !important;
         }
 
+        .product-admin-workspace nav[role="navigation"] [aria-current="page"] > span {
+            background-color: rgb(79 70 229) !important;
+            border-color: rgb(79 70 229) !important;
+            color: white !important;
+        }
+
         .product-admin-workspace nav[role="navigation"] span[aria-disabled="true"] > span {
             color: rgb(156 163 175) !important;
-            background-color: rgb(249 250 251) !important;
+            background-color: white !important;
         }
     </style>
 
