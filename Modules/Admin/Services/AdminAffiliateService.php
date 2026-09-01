8e791772373c66d92143c0799de501e899ea48a2
tungocvan <tungocvan@gmail.com>
2026-09-01 10:17:58 +0700

    fix(admin): point affiliate admin adapter to Order

--- Modules/Admin/Services/AdminAffiliateService.php
+++ Modules/Admin/Services/AdminAffiliateService.php
@@ -5,9 +5,9 @@ declare(strict_types=1);
 namespace Modules\Admin\Services;
 
 /**
- * @deprecated Canonical Affiliate commission orchestration is owned by Modules\Website.
+ * @deprecated Canonical Affiliate commission orchestration is owned by Modules\Order.
  * This compatibility adapter contains no independent business logic.
  */
-class AdminAffiliateService extends \Modules\Website\Services\AdminAffiliateService
+class AdminAffiliateService extends \Modules\Order\Services\AdminAffiliateService
 {
 }
