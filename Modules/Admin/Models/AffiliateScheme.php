68ed48cc250d00167ce8eaf55251127666a3fde7
tungocvan <tungocvan@gmail.com>
2026-09-01 10:19:45 +0700

    style(admin): format affiliate model adapter

--- Modules/Admin/Models/AffiliateScheme.php
+++ Modules/Admin/Models/AffiliateScheme.php
@@ -8,6 +8,4 @@ namespace Modules\Admin\Models;
  * @deprecated Canonical Affiliate scheme persistence is owned by Modules\Order.
  * This compatibility adapter contains no independent persistence metadata or relations.
  */
-class AffiliateScheme extends \Modules\Order\Models\AffiliateScheme
-{
-}
+class AffiliateScheme extends \Modules\Order\Models\AffiliateScheme {}
