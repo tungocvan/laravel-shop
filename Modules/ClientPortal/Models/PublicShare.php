4671790a2aa94ea843386ee8372839000d79f469
tungocvan <tungocvan@gmail.com>
2026-09-01 08:45:47 +0700

    refactor(clientportal): keep legacy Muasamcong model aliases

--- Modules/ClientPortal/Models/PublicShare.php
+++ Modules/ClientPortal/Models/PublicShare.php
@@ -2,27 +2,11 @@
 
 namespace Modules\ClientPortal\Models;
 
-use Illuminate\Database\Eloquent\Model;
+use Modules\ClientPortal\Applications\Muasamcong\Models\PublicShare as MuasamcongPublicShare;
 
-class PublicShare extends Model
+/**
+ * @deprecated Use Modules\ClientPortal\Applications\Muasamcong\Models\PublicShare.
+ */
+class PublicShare extends MuasamcongPublicShare
 {
-    protected $table = 'client_portal_public_shares';
-
-    protected $guarded = [];
-
-    protected $casts = [
-        'payload' => 'array',
-        'expires_at' => 'datetime',
-        'revoked_at' => 'datetime',
-        'last_viewed_at' => 'datetime',
-    ];
-
-    public function isAvailable(): bool
-    {
-        if ($this->revoked_at !== null) {
-            return false;
-        }
-
-        return $this->expires_at === null || $this->expires_at->isFuture();
-    }
 }
