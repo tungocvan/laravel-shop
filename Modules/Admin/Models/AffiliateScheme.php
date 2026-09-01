0b9dc568ef444d1a6da7a24c57f59256fa84f337
tungocvan <tungocvan@gmail.com>
2026-09-01 10:14:20 +0700

    fix(admin): point affiliate scheme adapter to Order owner

--- Modules/Admin/Models/AffiliateScheme.php
+++ Modules/Admin/Models/AffiliateScheme.php
@@ -5,9 +5,9 @@ declare(strict_types=1);
 namespace Modules\Admin\Models;
 
 /**
- * @deprecated Canonical Affiliate scheme persistence is owned by Modules\Website.
+ * @deprecated Canonical Affiliate scheme persistence is owned by Modules\Order.
  * This compatibility adapter contains no independent persistence metadata or relations.
  */
-class AffiliateScheme extends \Modules\Website\Models\AffiliateScheme
+class AffiliateScheme extends \Modules\Order\Models\AffiliateScheme
 {
 }
