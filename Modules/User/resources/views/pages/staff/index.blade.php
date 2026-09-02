2b96d4be950bd1f9d42d397dcc008860844961a8
tungocvan <tungocvan@gmail.com>
2026-09-02 12:45:51 +0700

    refactor(user): keep staff page as thin shell

--- Modules/User/resources/views/pages/staff/index.blade.php
+++ Modules/User/resources/views/pages/staff/index.blade.php
@@ -2,13 +2,7 @@
 @section('title', 'Quản lý Nhân sự')
 
 @section('content')
-    <div class="mx-auto w-full max-w-7xl space-y-6 px-4 py-6 sm:px-6 lg:px-8">
-        @livewire('shared.import-export.panel', [
-            'serviceClass' => \Modules\User\Services\ImportExport::class,
-            'title' => 'Import / Export Nhân sự',
-            'description' => 'Import nhân sự từ Excel theo email hoặc export danh sách nhân sự hiện tại.',
-        ])
-
+    <div class="w-full space-y-6 px-4 py-6 sm:px-6 lg:px-8">
         @livewire('user.user-table')
     </div>
 @endsection
