940458aa1bd7849b14b3abfa0155f56371c9ff59
tungocvan <tungocvan@gmail.com>
2026-09-03 16:21:30 +0700

    feat(muasamcong): add reusable KQLCNT workbook sheet export

---
+++ Modules/Muasamcong/Exports/KqlcntArraySheetExport.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Modules\Muasamcong\Exports;
+
+use Maatwebsite\Excel\Concerns\FromArray;
+use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithTitle;
+
+class KqlcntArraySheetExport implements FromArray, WithHeadings, WithTitle, ShouldAutoSize
+{
+    public function __construct(
+        private readonly string $title,
+        private readonly array $headings,
+        private readonly array $rows,
+    ) {}
+
+    public function array(): array
+    {
+        return $this->rows;
+    }
+
+    public function headings(): array
+    {
+        return $this->headings;
+    }
+
+    public function title(): string
+    {
+        return $this->title;
+    }
+}
