5f9a9994397ae11b6253513146d46fe8380f1006
tungocvan <tungocvan@gmail.com>
2026-09-04 10:45:36 +0700

    test(muasamcong): cover contextual KQLCNT supplement routes

--- tests/Feature/Muasamcong/KqlcntHistoricalRecoveryRouteTest.php
+++ tests/Feature/Muasamcong/KqlcntHistoricalRecoveryRouteTest.php
@@ -11,30 +11,34 @@ class KqlcntHistoricalRecoveryRouteTest extends TestCase
     {
         $view = Route::getRoutes()->getByName('muasamcong.contractors.kqlcnt-recovery');
         $template = Route::getRoutes()->getByName('muasamcong.contractors.kqlcnt-recovery.template');
+        $supplement = Route::getRoutes()->getByName('muasamcong.contractors.kqlcnt-recovery.supplement');
+        $supplementDownload = Route::getRoutes()->getByName('muasamcong.contractors.kqlcnt-recovery.supplement.download');
         $export = Route::getRoutes()->getByName('muasamcong.contractors.kqlcnt-recovery.export');
         $enrich = Route::getRoutes()->getByName('muasamcong.contractors.kqlcnt-recovery.enrich');
         $upload = Route::getRoutes()->getByName('muasamcong.contractors.kqlcnt-recovery.upload');
+        $supplementUpload = Route::getRoutes()->getByName('muasamcong.contractors.kqlcnt-recovery.supplement.upload');
         $batch = Route::getRoutes()->getByName('muasamcong.contractors.kqlcnt-recovery.batch');
         $preview = Route::getRoutes()->getByName('muasamcong.contractors.kqlcnt-recovery.preview');
         $confirm = Route::getRoutes()->getByName('muasamcong.contractors.kqlcnt-recovery.confirm');
 
-        foreach ([$view, $template, $export, $enrich, $upload, $batch, $preview, $confirm] as $route) {
+        foreach ([$view, $template, $supplement, $supplementDownload, $export, $enrich, $upload, $supplementUpload, $batch, $preview, $confirm] as $route) {
             $this->assertNotNull($route);
             $this->assertContains('auth:admin', $route->gatherMiddleware());
             $this->assertContains('permission:view_muasamcong,admin', $route->gatherMiddleware());
         }
 
-        foreach ([$enrich, $upload, $batch, $preview, $confirm] as $route) {
+        foreach ([$enrich, $upload, $supplementUpload, $batch, $preview, $confirm] as $route) {
             $this->assertContains('permission:muasamcong.pricing.sync,admin', $route->gatherMiddleware());
         }
 
-        foreach ([$view, $template, $export] as $route) {
+        foreach ([$view, $template, $supplement, $supplementDownload, $export] as $route) {
             $this->assertNotContains('permission:muasamcong.pricing.sync,admin', $route->gatherMiddleware());
         }
 
-        $this->assertSame('admin/muasamcong/contractors/history/{contractorSearch}/kqlcnt-recovery', $view->uri());
         $this->assertSame(['GET', 'HEAD'], $view->methods());
-        $this->assertSame(['GET', 'HEAD'], $template->methods());
+        $this->assertSame(['GET', 'HEAD'], $supplement->methods());
+        $this->assertSame(['GET', 'HEAD'], $supplementDownload->methods());
+        $this->assertSame(['POST'], $supplementUpload->methods());
         $this->assertSame(['POST'], $export->methods());
         $this->assertSame(['POST'], $enrich->methods());
         $this->assertSame(['POST'], $upload->methods());
