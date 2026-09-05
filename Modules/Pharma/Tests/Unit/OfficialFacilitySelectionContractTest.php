4d87e8f3a8722a4e76b812e48478a23d52bfe2d4
tungocvan <tungocvan@gmail.com>
2026-09-05 21:23:12 +0700

    test(pharma): cover direct checked-row import UX

--- Modules/Pharma/Tests/Unit/OfficialFacilitySelectionContractTest.php
+++ Modules/Pharma/Tests/Unit/OfficialFacilitySelectionContractTest.php
@@ -18,14 +18,29 @@ class OfficialFacilitySelectionContractTest extends TestCase
     }
 
     #[Test]
-    public function workspace_has_bounded_pagination_and_page_scoped_selection(): void
+    public function workspace_has_bounded_pagination_and_direct_page_scoped_import(): void
     {
         $view = file_get_contents(base_path('Modules/Pharma/resources/views/pages/official-facilities/import.blade.php'));
 
         $this->assertStringContainsString('[10,25,50,100]', $view);
         $this->assertStringNotContainsString('>All<', $view);
         $this->assertStringContainsString('name="visible[]"', $view);
+        $this->assertStringContainsString('name="selected[]"', $view);
         $this->assertStringContainsString('data-page-select', $view);
+        $this->assertStringContainsString('data-selected-count', $view);
+        $this->assertStringContainsString('admin.pharma.official-facilities.run', $view);
+        $this->assertStringNotContainsString('Lưu lựa chọn', $view);
+    }
+
+    #[Test]
+    public function direct_import_revalidates_selection_on_the_server(): void
+    {
+        $controller = file_get_contents(base_path('Modules/Pharma/Http/Controllers/OfficialFacilityImportController.php'));
+
+        $this->assertStringContainsString("'visible' => ['required', 'array', 'min:1']", $controller);
+        $this->assertStringContainsString("'selected' => ['required', 'array', 'min:1']", $controller);
+        $this->assertStringContainsString('intersect($visible)', $controller);
+        $this->assertStringContainsString("whereNull('imported_at')->update(['is_selected' => false])", $controller);
     }
 
     #[Test]
