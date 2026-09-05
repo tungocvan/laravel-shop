5b4ecf3465e279014636ae6344b0af4e3f8fd924
tungocvan <tungocvan@gmail.com>
2026-09-05 12:36:34 +0700

    refactor(pharma): remove duplicate cancellation panel

--- Modules/Pharma/resources/views/pages/drug-bid-award/allocations.blade.php
+++ Modules/Pharma/resources/views/pages/drug-bid-award/allocations.blade.php
@@ -10,6 +10,5 @@
             <a href="{{ route('admin.partner.partners.create', ['legal_type' => 'hospital']) }}" class="inline-flex min-h-10 items-center rounded-xl border border-slate-300 bg-white px-4 py-2 text-sm font-semibold text-slate-700 hover:bg-slate-50">+ Thêm bệnh viện</a>
         </div>
         @livewire('pharma.drug-bid-award.allocation-workspace', ['awardId' => $id])
-        @livewire('pharma.drug-bid-award.allocation-cancellation-panel', ['awardId' => $id])
     </div>
 @endsection
