5fabedb882c7234d4d986dec3a7332f5497f06ab
tungocvan <tungocvan@gmail.com>
2026-09-06 09:30:28 +0700

    test(pharma): cover source search filters and sync status polling

--- Modules/Pharma/Tests/Unit/OfficialSourceSyncContractTest.php
+++ Modules/Pharma/Tests/Unit/OfficialSourceSyncContractTest.php
@@ -8,15 +8,21 @@ use Tests\TestCase;
 class OfficialSourceSyncContractTest extends TestCase
 {
     #[Test]
-    public function source_sync_has_separate_permission_and_queue_job(): void
+    public function source_sync_has_separate_permission_queue_job_and_status_route(): void
     {
         $routes = file_get_contents(base_path('Modules/Pharma/routes/web.php'));
         $config = file_get_contents(base_path('Modules/Pharma/config/module.php'));
         $job = file_get_contents(base_path('Modules/Pharma/Jobs/PersistOfficialSourceSnapshotJob.php'));
+        $controller = file_get_contents(base_path('Modules/Pharma/Http/Controllers/OfficialSourceSyncController.php'));
+        $bhxhView = file_get_contents(base_path('Modules/Pharma/resources/views/pages/official-facilities/bhxh.blade.php'));
 
         $this->assertStringContainsString('sync_pharma_official_facilities', $routes);
         $this->assertStringContainsString('sync_pharma_official_facilities', $config);
         $this->assertStringContainsString('implements ShouldQueue', $job);
+        $this->assertStringContainsString("official-facilities.source.sync-status", $routes);
+        $this->assertStringContainsString('public function status(OfficialSourceSyncBatch $batch)', $controller);
+        $this->assertStringContainsString('pollSyncStatus', $bhxhView);
+        $this->assertStringContainsString('Đồng bộ hoàn tất', $bhxhView);
     }
 
     #[Test]
@@ -30,13 +36,19 @@ class OfficialSourceSyncContractTest extends TestCase
     }
 
     #[Test]
-    public function workspace_keeps_bounded_pagination_and_source_identity(): void
+    public function workspace_uses_shared_search_across_location_fields_and_keeps_bounded_pagination(): void
     {
         $view = file_get_contents(base_path('Modules/Pharma/resources/views/pages/official-facilities/source.blade.php'));
+        $controller = file_get_contents(base_path('Modules/Pharma/Http/Controllers/OfficialSourceSyncController.php'));
         $migration = file_get_contents(base_path('Modules/Pharma/database/migrations/2026_09_06_081000_create_official_source_facilities_table.php'));
 
-        $this->assertStringContainsString('[10,25,50,100]', $view);
+        $this->assertStringContainsString('<x-search', $view);
+        $this->assertStringContainsString("name=\"search\"", $view);
+        $this->assertStringContainsString('[10, 25, 50, 100]', $view);
         $this->assertStringNotContainsString('>All<', $view);
+        $this->assertStringContainsString("->orWhere('province_name', 'like', \$like)", $controller);
+        $this->assertStringContainsString("->orWhere('district_name', 'like', \$like)", $controller);
+        $this->assertStringContainsString('->withQueryString()', $controller);
         $this->assertStringContainsString("['source', 'external_id']", $migration);
         $this->assertStringContainsString('source_details', $migration);
     }
