d7800c6fa98fe28bad3a4c385892ee31903f779d
tungocvan <tungocvan@gmail.com>
2026-09-06 08:03:23 +0700

    feat(pharma): add official source mirror routes

--- Modules/Pharma/routes/web.php
+++ Modules/Pharma/routes/web.php
@@ -5,6 +5,7 @@ use Modules\Pharma\Http\Controllers\BhxhOfficialFacilityLookupController;
 use Modules\Pharma\Http\Controllers\DrugBidAwardController;
 use Modules\Pharma\Http\Controllers\OfficialFacilityImportController;
 use Modules\Pharma\Http\Controllers\OfficialFacilityImportTemplateController;
+use Modules\Pharma\Http\Controllers\OfficialSourceSyncController;
 use Modules\Pharma\Http\Controllers\PharmaController;
 use Modules\Pharma\Http\Controllers\PharmaDashboardController;
 use Modules\Pharma\Http\Controllers\PriceListController;
@@ -16,10 +17,15 @@ Route::prefix('admin/pharma')->name('admin.pharma.')->middleware(['web', 'auth:a
     Route::middleware('can:view_pharma_official_facilities')->group(function () {
         Route::get('/official-facilities/import', [OfficialFacilityImportController::class, 'index'])->name('official-facilities.index');
         Route::get('/official-facilities/import/template', OfficialFacilityImportTemplateController::class)->name('official-facilities.template');
+        Route::get('/official-facilities/source', [OfficialSourceSyncController::class, 'index'])->name('official-facilities.source.index');
         Route::get('/official-facilities/bhxh', [BhxhOfficialFacilityLookupController::class, 'index'])->name('official-facilities.bhxh.index');
         Route::get('/official-facilities/bhxh/captcha', [BhxhOfficialFacilityLookupController::class, 'captcha'])->name('official-facilities.bhxh.captcha');
         Route::get('/official-facilities/bhxh/districts', [BhxhOfficialFacilityLookupController::class, 'districts'])->name('official-facilities.bhxh.districts');
         Route::post('/official-facilities/bhxh/lookup', [BhxhOfficialFacilityLookupController::class, 'lookup'])->name('official-facilities.bhxh.lookup');
+    });
+
+    Route::middleware('can:sync_pharma_official_facilities')->group(function () {
+        Route::post('/official-facilities/source/sync', [OfficialSourceSyncController::class, 'store'])->name('official-facilities.source.sync');
     });
 
     Route::middleware('can:import_pharma_official_facilities')->group(function () {
