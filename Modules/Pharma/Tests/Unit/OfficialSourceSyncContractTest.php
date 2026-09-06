ae5c912245f7f8966cd2cb8fee281f162316343d
tungocvan <tungocvan@gmail.com>
2026-09-06 09:48:09 +0700

    test(pharma): cover live source filters

--- Modules/Pharma/Tests/Unit/OfficialSourceSyncContractTest.php
+++ Modules/Pharma/Tests/Unit/OfficialSourceSyncContractTest.php
@@ -36,14 +36,20 @@ class OfficialSourceSyncContractTest extends TestCase
     }
 
     #[Test]
-    public function workspace_uses_shared_search_across_location_fields_and_keeps_bounded_pagination(): void
+    public function workspace_uses_shared_search_live_filters_and_keeps_bounded_pagination(): void
     {
         $view = file_get_contents(base_path('Modules/Pharma/resources/views/pages/official-facilities/source.blade.php'));
         $controller = file_get_contents(base_path('Modules/Pharma/Http/Controllers/OfficialSourceSyncController.php'));
         $migration = file_get_contents(base_path('Modules/Pharma/database/migrations/2026_09_06_081000_create_official_source_facilities_table.php'));
 
         $this->assertStringContainsString('<x-search', $view);
-        $this->assertStringContainsString("name=\"search\"", $view);
+        $this->assertStringContainsString('name="search"', $view);
+        $this->assertStringContainsString('data-live-filter-form', $view);
+        $this->assertStringContainsString('data-live-search', $view);
+        $this->assertStringContainsString('data-live-filter', $view);
+        $this->assertStringContainsString("filter.addEventListener('change', submitFilters)", $view);
+        $this->assertStringContainsString('window.setTimeout(submitFilters, 450)', $view);
+        $this->assertStringNotContainsString('>Lọc</button>', $view);
         $this->assertStringContainsString('[10, 25, 50, 100]', $view);
         $this->assertStringNotContainsString('>All<', $view);
         $this->assertStringContainsString("->orWhere('province_name', 'like', \$like)", $controller);
