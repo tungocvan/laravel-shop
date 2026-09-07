d8da9da4bf782bd4ab5403b56df5f49b0dfb53eb
tungocvan <tungocvan@gmail.com>
2026-09-07 10:38:42 +0700

    style(partner): normalize module config formatting

--- Modules/Partner/config/module.php
+++ Modules/Partner/config/module.php
@@ -1,18 +1,16 @@
 <?php
 
-return array (
-  'name' => 'Partner',
-  'type' => 'domain',
-  'enabled' => false,
-  'permissions' => 
-  array (
-    0 => 'view_partner',
-    1 => 'create_partner',
-    2 => 'edit_partner',
-    3 => 'delete_partner',
-  ),
-  'tables' => 
-  array (
-    0 => 'partners',
-  ),
-);
+return [
+    'name' => 'Partner',
+    'type' => 'domain',
+    'enabled' => false,
+    'permissions' => [
+        'view_partner',
+        'create_partner',
+        'edit_partner',
+        'delete_partner',
+    ],
+    'tables' => [
+        'partners',
+    ],
+];
