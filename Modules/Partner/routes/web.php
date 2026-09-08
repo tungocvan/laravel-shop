f3eb26f10af8c3cb76ab43a28cf0afd5fbbc4718
tungocvan <tungocvan@gmail.com>
2026-09-08 10:30:25 +0700

    Partner: expose invoice candidate review route

--- Modules/Partner/routes/web.php
+++ Modules/Partner/routes/web.php
@@ -9,6 +9,7 @@ Route::prefix('admin/partners')
     ->group(function () {
         Route::view('/dashboard', 'partner::pages.dashboard')->name('dashboard');
         Route::view('/business-lookup', 'partner::pages.business-lookup')->name('lookup');
+        Route::view('/sync/invoices', 'partner::pages.invoice-candidates')->name('invoice-candidates');
         Route::get('/', [PartnerController::class, 'index'])->name('index');
         Route::get('/create', [PartnerController::class, 'create'])->name('create');
         Route::get('/{id}/edit', [PartnerController::class, 'edit'])->name('edit');
