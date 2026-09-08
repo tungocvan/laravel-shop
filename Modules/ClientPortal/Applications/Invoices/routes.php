f30776449c4e9a1439c8504714d41c52f0712d7e
tungocvan <tungocvan@gmail.com>
2026-09-09 05:19:13 +0700

    feat(clientportal): add invoices PWA routes

---
+++ Modules/ClientPortal/Applications/Invoices/routes.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Modules\ClientPortal\Applications\Invoices\Http\Controllers\InvoicesApplicationController;
+
+if ((bool) config('modules.registry.Invoices.enabled', false)) {
+    Route::middleware([
+        'web',
+        'auth:web',
+        'client.application:invoices',
+    ])->prefix('apps/invoices')->name('client.invoices.')->group(function (): void {
+        Route::get('/', [InvoicesApplicationController::class, 'dashboard'])
+            ->middleware('client.feature:invoices,overview')
+            ->name('dashboard');
+
+        Route::get('/list', [InvoicesApplicationController::class, 'index'])
+            ->middleware('client.feature:invoices,list')
+            ->name('index');
+        Route::get('/list/{invoice}', [InvoicesApplicationController::class, 'show'])
+            ->middleware('client.feature:invoices,list')
+            ->name('show');
+        Route::post('/export', [InvoicesApplicationController::class, 'export'])
+            ->middleware('client.feature:invoices,list')
+            ->name('export');
+        Route::get('/list/{invoice}/pdf', [InvoicesApplicationController::class, 'pdf'])
+            ->middleware('client.feature:invoices,list')
+            ->name('pdf');
+
+        Route::get('/sync', [InvoicesApplicationController::class, 'sync'])
+            ->middleware('client.feature:invoices,sync')
+            ->name('sync');
+        Route::post('/sync', [InvoicesApplicationController::class, 'startSync'])
+            ->middleware(['client.feature:invoices,sync', 'throttle:10,1'])
+            ->name('sync.start');
+    });
+}
