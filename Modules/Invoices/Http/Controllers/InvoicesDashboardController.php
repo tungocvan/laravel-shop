64232f1ad7ea382f59ceb52fe3f407f3477e8cfc
tungocvan <tungocvan@gmail.com>
2026-09-08 15:47:39 +0700

    Invoices: expose Google Drive health on dashboard

--- Modules/Invoices/Http/Controllers/InvoicesDashboardController.php
+++ Modules/Invoices/Http/Controllers/InvoicesDashboardController.php
@@ -5,17 +5,39 @@ namespace Modules\Invoices\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Contracts\View\View;
 use Modules\Invoices\Services\InvoiceDashboardService;
+use Modules\System\Services\Cloud\GoogleDriveConnectionService;
+use Throwable;
 
 final class InvoicesDashboardController extends Controller
 {
-    public function __invoke(InvoiceDashboardService $dashboard): View
+    public function __invoke(InvoiceDashboardService $dashboard, GoogleDriveConnectionService $drive): View
     {
         $admin = auth('admin')->user();
 
         abort_unless($admin !== null, 403);
 
+        try {
+            $driveStatus = array_merge([
+                'available' => true,
+                'connected' => false,
+                'email' => '',
+                'folder_name' => '',
+                'last_checked_at' => '',
+            ], $drive->status());
+        } catch (Throwable $exception) {
+            report($exception);
+            $driveStatus = [
+                'available' => false,
+                'connected' => false,
+                'email' => '',
+                'folder_name' => '',
+                'last_checked_at' => '',
+            ];
+        }
+
         return view('Invoices::pages.invoices.dashboard', [
             'dashboard' => $dashboard->forUser($admin),
+            'driveStatus' => $driveStatus,
         ]);
     }
 }
