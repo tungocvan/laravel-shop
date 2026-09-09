8d387e984102084c2ea7b38d5887337167918e72
tungocvan <tungocvan@gmail.com>
2026-09-09 14:59:01 +0700

    fix(inventory): enforce item fractional quantity policy on transfers

--- Modules/Inventory/Services/TransferPostingService.php
+++ Modules/Inventory/Services/TransferPostingService.php
@@ -59,6 +59,10 @@ class TransferPostingService
                     throw new DomainException('Transfer quantity must be positive.');
                 }
 
+                if (! $item->allow_fractional_quantity && DecimalQuantity::hasFractionalPart((string) $line->base_quantity)) {
+                    throw new DomainException('Fractional quantity is not allowed for this inventory item.');
+                }
+
                 if ($line->base_uom !== $item->base_uom) {
                     throw new DomainException('Transfer line base UOM must match the inventory item base UOM.');
                 }
