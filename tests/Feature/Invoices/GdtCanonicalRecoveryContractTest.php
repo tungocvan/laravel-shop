2080836e09c844f0f94549dd33627ecdd1389a2b
tungocvan <tungocvan@gmail.com>
2026-09-10 10:21:39 +0700

    test(invoices): avoid regex interpolation in canonical recovery contract

--- tests/Feature/Invoices/GdtCanonicalRecoveryContractTest.php
+++ tests/Feature/Invoices/GdtCanonicalRecoveryContractTest.php
@@ -14,10 +14,10 @@ class GdtCanonicalRecoveryContractTest extends TestCase
         $job = file_get_contents(base_path('Modules/Invoices/Jobs/ProcessGdtInvoicesJob.php'));
 
         $this->assertStringContainsString('recoverMissingDetailsFromLocalRange', $service);
-        $this->assertMatchesRegularExpression("/~>where\\('invoice_type',\\s*\\$invoiceType\\)/", str_replace('->where', '~>where', $service));
-        $this->assertMatchesRegularExpression("/~>where\\('detail_status',\\s*'!=',\\s*'READY'\\)/", str_replace('->where', '~>where', $service));
-        $this->assertMatchesRegularExpression('/acquireMissingDetails\\(\\$invoiceIds\\s*,/', $service);
-        $this->assertMatchesRegularExpression('/\\$service->recoverMissingDetailsFromLocalRange\\s*\\(/', $job);
+        $this->assertMatchesRegularExpression('/~>where\(\'invoice_type\',\s*\$invoiceType\)/', str_replace('->where', '~>where', $service));
+        $this->assertMatchesRegularExpression('/~>where\(\'detail_status\',\s*\'!=\',\s*\'READY\'\)/', str_replace('->where', '~>where', $service));
+        $this->assertMatchesRegularExpression('/acquireMissingDetails\(\$invoiceIds\s*,/', $service);
+        $this->assertMatchesRegularExpression('/\$service->recoverMissingDetailsFromLocalRange\s*\(/', $job);
         $this->assertStringContainsString('RAW canonical sau recovery detail', $job);
     }
 
@@ -26,10 +26,10 @@ class GdtCanonicalRecoveryContractTest extends TestCase
     {
         $job = file_get_contents(base_path('Modules/Invoices/Jobs/ProcessGdtInvoicesJob.php'));
 
-        $this->assertMatchesRegularExpression('/\\$detailReady\\s*=/', $job);
-        $this->assertMatchesRegularExpression('/&&\\s*\\$detailReady\\s*&&\\s*!\\s*\\$canonicalReady/', $job);
-        $this->assertMatchesRegularExpression("/'source'\\s*=>\\s*'local_detail_recovery'/", $job);
-        $this->assertMatchesRegularExpression("/'source_header_pending'\\s*=>\\s*true/", $job);
+        $this->assertMatchesRegularExpression('/\$detailReady\s*=/', $job);
+        $this->assertMatchesRegularExpression('/&&\s*\$detailReady\s*&&\s*!\s*\$canonicalReady/', $job);
+        $this->assertMatchesRegularExpression('/\'source\'\s*=>\s*\'local_detail_recovery\'/', $job);
+        $this->assertMatchesRegularExpression('/\'source_header_pending\'\s*=>\s*true/', $job);
         $this->assertStringContainsString('Inventory có thể chuẩn hóa từ local', $job);
     }
 
@@ -39,6 +39,6 @@ class GdtCanonicalRecoveryContractTest extends TestCase
         $job = file_get_contents(base_path('Modules/Invoices/Jobs/ProcessGdtInvoicesJob.php'));
 
         $this->assertStringContainsString('public array $backoff = [60, 180];', $job);
-        $this->assertMatchesRegularExpression("/'attempt'\\s*=>\\s*\\$this->attempts\\(\\)/", $job);
+        $this->assertMatchesRegularExpression('/\'attempt\'\s*=>\s*\$this->attempts\(\)/', $job);
     }
 }
