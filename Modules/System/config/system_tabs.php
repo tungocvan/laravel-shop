68aca7ba8a0605af7f592bd844aca9d7c02c72e5
tungocvan <tungocvan@gmail.com>
2026-09-13 10:17:39 +0700

    fix(system): normalize database tab icon path

--- Modules/System/config/system_tabs.php
+++ Modules/System/config/system_tabs.php
@@ -18,7 +18,7 @@ return [
     [
         'id' => 'database',
         'label' => 'Kết nối Database',
-        'icon' => '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6"><path stroke-linecap="round" stroke-linejoin="round" d="m20.25 7.5-.625 10.632a2.25 2.25 0 0 1-2.247 2.118H6.622a2.25 2.25 0 0 1-2.247-2.118L3.75 7.5M10 11.25h4M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125Z" /></svg>',
+        'icon' => 'M4 7v10c0 2.21 3.58 4 8 4s8-1.79 8-4V7M4 7c0 2.21 3.58 4 8 4s8-1.79 8-4M4 7c0-2.21 3.58-4 8-4s8 1.79 8 4',
         'component' => 'system.settings.database-config',
         'enabled' => true,
     ],
