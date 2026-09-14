d82449a6a6caab1ce21e26a81d31fbd9c0209f62
tungocvan <tungocvan@gmail.com>
2026-09-14 12:24:23 +0700

    test(pharma): cover clear medicine import history action

--- Modules/Pharma/Tests/Unit/MedicineCatalogImportUiContractTest.php
+++ Modules/Pharma/Tests/Unit/MedicineCatalogImportUiContractTest.php
@@ -22,18 +22,25 @@ class MedicineCatalogImportUiContractTest extends TestCase
     }
 
     #[Test]
-    public function import_workspace_has_template_auto_filters_and_timestamped_history(): void
+    public function import_workspace_has_template_auto_filters_timestamped_history_and_safe_clear_action(): void
     {
         $routes = file_get_contents(base_path('Modules/Pharma/routes/web.php'));
         $view = file_get_contents(base_path('Modules/Pharma/resources/views/pages/medicine-import/index.blade.php'));
+        $controller = file_get_contents(base_path('Modules/Pharma/Http/Controllers/MedicineCatalogImportController.php'));
         $export = file_get_contents(base_path('Modules/Pharma/Exports/MedicineCatalogTemplateExport.php'));
 
         $this->assertStringContainsString("name('import.template')", $routes);
+        $this->assertStringContainsString("name('import.history.clear')", $routes);
         $this->assertStringContainsString("route('admin.pharma.medicines.import.template')", $view);
+        $this->assertStringContainsString("route('admin.pharma.medicines.import.history.clear')", $view);
         $this->assertStringContainsString('Tải template danh mục thuốc (.xlsx)', $view);
+        $this->assertStringContainsString('Clear lịch sử import', $view);
         $this->assertStringContainsString('onchange="this.form.submit()"', $view);
         $this->assertStringNotContainsString('Áp dụng bộ lọc', $view);
         $this->assertStringContainsString("format('d/m/Y H:i')", $view);
+        $this->assertStringContainsString('MedicineImportRow::query()->delete()', $controller);
+        $this->assertStringContainsString('MedicineImportBatch::query()->delete()', $controller);
+        $this->assertStringContainsString('Medicine Master đã đồng bộ không bị thay đổi', $controller);
         $this->assertStringContainsString("'Tên hoạt chất'", $export);
         $this->assertStringContainsString("'Nồng độ - Hàm lượng'", $export);
         $this->assertStringContainsString("'Tên biệt dược'", $export);
