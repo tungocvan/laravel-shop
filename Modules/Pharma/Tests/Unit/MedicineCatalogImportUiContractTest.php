3a6231b846f5256486f750f0c20c1dc927d0dcec
tungocvan <tungocvan@gmail.com>
2026-09-14 11:42:47 +0700

    test(pharma): assert staged import workspace contract

--- Modules/Pharma/Tests/Unit/MedicineCatalogImportUiContractTest.php
+++ Modules/Pharma/Tests/Unit/MedicineCatalogImportUiContractTest.php
@@ -10,11 +10,14 @@ class MedicineCatalogImportUiContractTest extends TestCase
     #[Test]
     public function medicine_master_uses_staged_import_workspace_instead_of_legacy_direct_import_panel(): void
     {
-        $view = file_get_contents(base_path('Modules/Pharma/resources/views/livewire/medicine/index.blade.php'));
+        $masterView = file_get_contents(base_path('Modules/Pharma/resources/views/livewire/medicine/index.blade.php'));
+        $importView = file_get_contents(base_path('Modules/Pharma/resources/views/pages/medicine-import/index.blade.php'));
 
-        $this->assertIsString($view);
-        $this->assertStringContainsString("route('admin.pharma.medicines.import.index')", $view);
-        $this->assertStringContainsString('Import an toàn qua staging', $view);
-        $this->assertStringNotContainsString("@livewire('shared.import-export.panel'", $view);
+        $this->assertIsString($masterView);
+        $this->assertIsString($importView);
+        $this->assertStringContainsString("route('admin.pharma.medicines.import.index')", $masterView);
+        $this->assertStringContainsString('Upload chỉ tạo staging', $importView);
+        $this->assertStringContainsString('Preview staging', $importView);
+        $this->assertStringNotContainsString("@livewire('shared.import-export.panel'", $masterView);
     }
 }
