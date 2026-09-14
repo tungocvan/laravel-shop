8ac2df6d95062d1c6054ca1693ab53e54d2cce06
tungocvan <tungocvan@gmail.com>
2026-09-14 12:16:48 +0700

    test(pharma): cover import template filters and history

--- Modules/Pharma/Tests/Unit/MedicineCatalogImportUiContractTest.php
+++ Modules/Pharma/Tests/Unit/MedicineCatalogImportUiContractTest.php
@@ -20,4 +20,22 @@ class MedicineCatalogImportUiContractTest extends TestCase
         $this->assertStringContainsString('Preview staging', $importView);
         $this->assertStringNotContainsString("@livewire('shared.import-export.panel'", $masterView);
     }
+
+    #[Test]
+    public function import_workspace_has_template_auto_filters_and_timestamped_history(): void
+    {
+        $routes = file_get_contents(base_path('Modules/Pharma/routes/web.php'));
+        $view = file_get_contents(base_path('Modules/Pharma/resources/views/pages/medicine-import/index.blade.php'));
+        $export = file_get_contents(base_path('Modules/Pharma/Exports/MedicineCatalogTemplateExport.php'));
+
+        $this->assertStringContainsString("name('import.template')", $routes);
+        $this->assertStringContainsString("route('admin.pharma.medicines.import.template')", $view);
+        $this->assertStringContainsString('Tải template danh mục thuốc (.xlsx)', $view);
+        $this->assertStringContainsString('onchange="this.form.submit()"', $view);
+        $this->assertStringNotContainsString('Áp dụng bộ lọc', $view);
+        $this->assertStringContainsString("format('d/m/Y H:i')", $view);
+        $this->assertStringContainsString("'Tên hoạt chất'", $export);
+        $this->assertStringContainsString("'Nồng độ - Hàm lượng'", $export);
+        $this->assertStringContainsString("'Tên biệt dược'", $export);
+    }
 }
