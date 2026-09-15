f9366cd3fd20792be52e2a6a4aefbf6b7a76aaa1
tungocvan <tungocvan@gmail.com>
2026-09-15 11:33:39 +0700

    test(pharma): align price list bid UI contract with table-first workspace

--- Modules/Pharma/Tests/Unit/PriceListBidProfessionalUiContractTest.php
+++ Modules/Pharma/Tests/Unit/PriceListBidProfessionalUiContractTest.php
@@ -10,23 +10,22 @@ class PriceListBidProfessionalUiContractTest extends TestCase
     #[Test]
     public function price_list_uses_table_first_bid_intelligence_instead_of_expanded_award_cards(): void
     {
-        $view = file_get_contents(base_path('Modules/Pharma/resources/views/livewire/price-list/create.blade.php'));
-
+        $view = file_get_contents(base_path('Modules/Pharma/resources/views/livewire/price-list/workspace-bid.blade.php'));
         $this->assertStringContainsString('KQ trúng thầu', $view);
         $this->assertStringContainsString('Đã có', $view);
         $this->assertStringContainsString('Bổ sung', $view);
-        $this->assertStringContainsString("wire:click=\"showBidHistory", $view);
+        $this->assertStringContainsString('wire:click="showBidHistory', $view);
+        $this->assertStringContainsString('wire:click="openManualBid', $view);
         $this->assertStringNotContainsString('Kết quả trúng thầu của sản phẩm đã chọn', $view);
     }
 
     #[Test]
-    public function bid_history_is_shown_in_a_modal_and_never_writes_commercial_price_inputs(): void
+    public function responsive_workspace_keeps_bid_evidence_separate_from_commercial_prices(): void
     {
-        $view = file_get_contents(base_path('Modules/Pharma/resources/views/livewire/price-list/create.blade.php'));
+        $view = file_get_contents(base_path('Modules/Pharma/resources/views/livewire/price-list/workspace-bid.blade.php'));
         $component = file_get_contents(base_path('Modules/Pharma/Livewire/PriceList/Create.php'));
-
-        $this->assertStringContainsString('Lịch sử kết quả trúng thầu', $view);
-        $this->assertStringContainsString('selectBidAward(', $component);
+        $this->assertStringContainsString('md:hidden', $view);
+        $this->assertStringContainsString('lg:hidden', $view);
         $this->assertStringContainsString('selectedBidAwardIds', $component);
         $this->assertStringNotContainsString("prices[$key]['company'] = $award", $component);
         $this->assertStringNotContainsString("prices[$key]['receivable'] = $award", $component);
@@ -34,11 +33,20 @@ class PriceListBidProfessionalUiContractTest extends TestCase
     }
 
     #[Test]
+    public function manual_bid_is_audited_and_linked_to_canonical_master(): void
+    {
+        $workspace = file_get_contents(base_path('Modules/Pharma/Livewire/PriceList/Workspace.php'));
+        $this->assertStringContainsString('saveManualBid()', $workspace);
+        $this->assertStringContainsString("'source_type' => DrugBidAward::SOURCE_MANUAL", $workspace);
+        $this->assertStringContainsString("'created_by' => auth('admin')->id()", $workspace);
+        $this->assertStringContainsString('matchManager->confirm(', $workspace);
+    }
+
+    #[Test]
     public function create_and_edit_pages_are_full_width_workspaces(): void
     {
         $create = file_get_contents(base_path('Modules/Pharma/resources/views/pages/price-list/create.blade.php'));
         $edit = file_get_contents(base_path('Modules/Pharma/resources/views/pages/price-list/edit.blade.php'));
-
         $this->assertStringContainsString('w-full', $create);
         $this->assertStringContainsString('w-full', $edit);
         $this->assertStringNotContainsString('max-w-[1600px]', $edit);
