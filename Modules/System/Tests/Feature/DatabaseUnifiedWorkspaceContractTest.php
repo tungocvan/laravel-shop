3d91f459eb1276d6601633e347e9aaf316f8224f
tungocvan <tungocvan@gmail.com>
2026-09-17 20:46:17 +0700

    test(system): preserve google drive backup capabilities

--- Modules/System/Tests/Feature/DatabaseUnifiedWorkspaceContractTest.php
+++ Modules/System/Tests/Feature/DatabaseUnifiedWorkspaceContractTest.php
@@ -20,6 +20,21 @@ class DatabaseUnifiedWorkspaceContractTest extends TestCase
         self::assertStringContainsString('Danh sách bảng được ẩn mặc định', $view);
     }
 
+    public function test_unified_workspace_preserves_full_database_and_drive_manager(): void
+    {
+        $view = file_get_contents(base_path('Modules/System/resources/views/pages/database.blade.php'));
+        self::assertIsString($view);
+        self::assertStringContainsString('Toàn bộ Database · Backup / Restore · Google Drive', $view);
+        self::assertStringContainsString("@livewire('system.database.backup-manager')", $view);
+        self::assertStringContainsString('đồng bộ Google Drive hai chiều', $view);
+
+        $manager = file_get_contents(base_path('Modules/System/Livewire/Database/BackupManager.php'));
+        self::assertIsString($manager);
+        self::assertStringContainsString('GoogleDriveConnectionService', $manager);
+        self::assertStringContainsString('GoogleDriveBackupBrowserService', $manager);
+        self::assertStringContainsString('UploadDatabaseBackupToGoogleDrive', $manager);
+    }
+
     public function test_legacy_backup_restore_route_redirects_to_database_workspace(): void
     {
         $routes = file_get_contents(base_path('Modules/System/routes/web.php'));
