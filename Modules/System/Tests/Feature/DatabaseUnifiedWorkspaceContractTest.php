ba1d3dc7295bf384472c84155b4a7fa4a99ea728
tungocvan <tungocvan@gmail.com>
2026-09-17 20:39:44 +0700

    test(system): cover unified database workspace

---
+++ Modules/System/Tests/Feature/DatabaseUnifiedWorkspaceContractTest.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\System\Tests\Feature;
+
+use Tests\TestCase;
+
+class DatabaseUnifiedWorkspaceContractTest extends TestCase
+{
+    public function test_database_workspace_is_module_first_and_lazy_renders_table_workspace(): void
+    {
+        $view = file_get_contents(base_path('Modules/System/resources/views/pages/database.blade.php'));
+        self::assertIsString($view);
+        self::assertStringContainsString('Chọn Module cần Backup / Restore', $view);
+        self::assertStringContainsString('loadingModule = true', $view);
+        self::assertStringContainsString('Đang tải Module…', $view);
+        self::assertStringContainsString("@if (\$selectedModule === '')", $view);
+        self::assertStringContainsString("['moduleFilter' => \$selectedModule]", $view);
+        self::assertStringContainsString('Danh sách bảng được ẩn mặc định', $view);
+    }
+
+    public function test_legacy_backup_restore_route_redirects_to_database_workspace(): void
+    {
+        $routes = file_get_contents(base_path('Modules/System/routes/web.php'));
+        self::assertIsString($routes);
+        self::assertStringContainsString("Route::redirect('/backup-restore', '/admin/system/database')", $routes);
+        self::assertStringContainsString("->name('backup-restore')", $routes);
+    }
+
+    public function test_database_controller_limits_workspace_to_known_module(): void
+    {
+        $controller = file_get_contents(base_path('Modules/System/Http/Controllers/DatabaseController.php'));
+        self::assertIsString($controller);
+        self::assertStringContainsString("\$request->query('module', '')", $controller);
+        self::assertStringContainsString("\$module !== 'Unknown'", $controller);
+        self::assertStringContainsString('in_array($selectedModule, $modules, true)', $controller);
+    }
+}
