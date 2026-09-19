f1a279d99591d4b4710d7b10f02fa71606f0828e
tungocvan <tungocvan@gmail.com>
2026-09-19 08:38:21 +0700

    test: make dossier contract tests standalone PHPUnit compatible

--- Modules/System/Tests/Unit/GoogleDriveApplicationUploadContractTest.php
+++ Modules/System/Tests/Unit/GoogleDriveApplicationUploadContractTest.php
@@ -8,12 +8,12 @@ class GoogleDriveApplicationUploadContractTest extends TestCase
 {
     public function test_google_drive_connection_exposes_scoped_application_upload(): void
     {
-        $service = file_get_contents(base_path('Modules/System/Services/Cloud/GoogleDriveConnectionService.php'));
+        $service = file_get_contents(dirname(__DIR__, 4).'/Modules/System/Services/Cloud/GoogleDriveConnectionService.php');
 
         $this->assertStringContainsString('public function uploadApplicationFile(', $service);
         $this->assertStringContainsString('$this->resolveRootFolder($token)', $service);
         $this->assertStringContainsString('$this->ensureChildFolder($token, $parentId, $folder)', $service);
-        $this->assertStringContainsString("str_contains($folder, '/')", $service);
+        $this->assertStringContainsString("str_contains(\$folder, '/')", $service);
         $this->assertStringContainsString("'uploadType=media'", $service);
     }
 }
