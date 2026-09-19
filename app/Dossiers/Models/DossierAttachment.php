cd98c57753c0703abae1e09e1f5b39882ae3ce00
tungocvan <tungocvan@gmail.com>
2026-09-19 09:42:26 +0700

    feat(dossier): track remote attachment identity

--- app/Dossiers/Models/DossierAttachment.php
+++ app/Dossiers/Models/DossierAttachment.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class DossierAttachment extends Model
 {
-    protected $fillable = ['dossier_id', 'item_id', 'kind', 'disk', 'path', 'original_name', 'mime_type', 'size', 'checksum', 'sync_status', 'remote_path', 'uploaded_by'];
+    protected $fillable = ['dossier_id', 'item_id', 'kind', 'disk', 'path', 'original_name', 'mime_type', 'size', 'checksum', 'sync_status', 'remote_path', 'remote_id', 'uploaded_by'];
 
     public function dossier(): BelongsTo
     {
