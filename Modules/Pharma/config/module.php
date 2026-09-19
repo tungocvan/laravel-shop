73aaaba9671bb9705bdc9b367dc14c75433fc318
tungocvan <tungocvan@gmail.com>
2026-09-19 10:45:18 +0700

    fix(pharma): correct medicine profile class reference

--- Modules/Pharma/config/module.php
+++ Modules/Pharma/config/module.php
@@ -16,7 +16,7 @@ return [
                     'table' => 'dossiers',
                     'key' => 'id',
                     'where_column' => 'owner_type',
-                    'where_value' => Modules\\Pharma\\Models\\MedicineProfile::class,
+                    'where_value' => Modules\Pharma\Models\MedicineProfile::class,
                     'owner_table' => 'medicine_profiles',
                     'owner_key' => 'id',
                     'owner_foreign_key' => 'owner_id',
