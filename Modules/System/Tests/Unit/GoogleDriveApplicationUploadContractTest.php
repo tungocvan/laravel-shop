5c1edeb979a3f8d58f5cfd80153d00df859c6aa0
tungocvan <tungocvan@gmail.com>
2026-09-19 08:35:48 +0700

    test(system): cover scoped Google Drive application upload

---
+++ Modules/System/Tests/Unit/GoogleDriveApplicationUploadContractTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Modules\System\Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class GoogleDriveApplicationUploadContractTest extends TestCase
+{
+    public function test_google_drive_connection_exposes_scoped_application_upload(): void
+    {
+        $service = file_get_contents(base_path('Modules/System/Services/Cloud/GoogleDriveConnectionService.php'));
+
+        $this->assertStringContainsString('public function uploadApplicationFile(', $service);
+        $this->assertStringContainsString('$this->resolveRootFolder($token)', $service);
+        $this->assertStringContainsString('$this->ensureChildFolder($token, $parentId, $folder)', $service);
+        $this->assertStringContainsString("str_contains($folder, '/')", $service);
+        $this->assertStringContainsString("'uploadType=media'", $service);
+    }
+}
