b790dfec294009edb40820f30bf8c4e08a48112e
tungocvan <tungocvan@gmail.com>
2026-09-21 13:31:28 +0700

    fix(pharma): use full admin container for supplier tracking

--- Modules/Pharma/resources/views/pages/supplier-trackings/index.blade.php
+++ Modules/Pharma/resources/views/pages/supplier-trackings/index.blade.php
@@ -1,9 +1,10 @@
 @extends('Admin::layouts.master')
 
 @section('title', 'Pharma')
+@section('admin_container', 'full')
 
 @section('content')
-    <div class="container-fluid">
+    <div class="w-full">
         @include('Pharma::pages.partials.dashboard-back')
 
         @livewire('pharma.supplier-trackings.index')
