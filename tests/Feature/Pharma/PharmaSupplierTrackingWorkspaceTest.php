24532abd9b671f7707c59afbb8b705671c97b3f6
tungocvan <tungocvan@gmail.com>
2026-09-21 13:03:13 +0700

    test(pharma): fix literal supplier option assertions

--- tests/Feature/Pharma/PharmaSupplierTrackingWorkspaceTest.php
+++ tests/Feature/Pharma/PharmaSupplierTrackingWorkspaceTest.php
@@ -114,8 +114,8 @@ class PharmaSupplierTrackingWorkspaceTest extends TestCase
         $this->assertStringContainsString('Chọn hoặc tìm nhà cung cấp', $view);
         $this->assertStringContainsString("#[On('supplier-search')]", $form);
         $this->assertStringContainsString("public array \$supplierOptions = [];", $form);
-        $this->assertStringContainsString('refreshSupplierOptions(\$service)', $form);
-        $this->assertStringContainsString('\$this->supplierOptions = \$service->supplierCandidates', $form);
+        $this->assertStringContainsString('refreshSupplierOptions($service)', $form);
+        $this->assertStringContainsString('$this->supplierOptions = $service->supplierCandidates', $form);
         $selectSearch = file_get_contents(base_path('resources/views/components/select-search.blade.php'));
         $this->assertStringContainsString("optionsWire: @js(\$attributes->get('options-wire'))", $selectSearch);
         $this->assertStringNotContainsString("optionsWire: '{{ \$attributes->get('options-wire') }}'", $selectSearch);
