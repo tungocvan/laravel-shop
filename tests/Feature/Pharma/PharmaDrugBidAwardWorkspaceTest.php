bd009e47bcabccb447bde415cb7edf0c3eca394c
tungocvan <tungocvan@gmail.com>
2026-09-22 13:23:44 +0700

    test(pharma): align TBMT selection assertion with representative id

--- tests/Feature/Pharma/PharmaDrugBidAwardWorkspaceTest.php
+++ tests/Feature/Pharma/PharmaDrugBidAwardWorkspaceTest.php
@@ -23,7 +23,7 @@ class PharmaDrugBidAwardWorkspaceTest extends TestCase
         $this->assertStringContainsString('Sản phẩm / Phân bổ', $view);
         $this->assertStringNotContainsString('wire:model.live="selectedIds"', $view);
         $this->assertStringContainsString('x-model="selected"', $view);
-        $this->assertStringContainsString("x-on:change=\"\$wire.call('setAwardSelected', {{ \$award->id }}, \$event.target.checked)\"", $view);
+        $this->assertStringContainsString("x-on:change=\"\$wire.call('setAwardSelected', {{ \$award->representative_id }}, \$event.target.checked)\"", $view);
         $this->assertStringContainsString('public function setAwardSelected(mixed $id, bool $selected): void', $component);
         $this->assertStringContainsString('wire:key="award-checkbox-{{ $award->representative_id }}-', $view);
         $this->assertStringContainsString('public function updatedSelectedIds(): void', $component);
