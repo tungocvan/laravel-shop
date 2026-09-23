8a76ddf9d5e73ff522070a68251aa3a2f3a590f1
tungocvan <tungocvan@gmail.com>
2026-09-23 10:51:02 +0700

    fix(pharma): import database facade for inventory documents

--- Modules/Pharma/Http/Controllers/InventoryController.php
+++ Modules/Pharma/Http/Controllers/InventoryController.php
@@ -4,6 +4,7 @@ namespace Modules\Pharma\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\View\View;
 use Modules\Pharma\Models\InventoryBalance;
 use Modules\Pharma\Models\InventoryIssue;
