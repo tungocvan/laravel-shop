2453871b76109f8fdb396049ef2b0ed9becd6397
tungocvan <tungocvan@gmail.com>
2026-09-24 10:46:40 +0700

    fix(pharma): avoid interpolation in bid post guard assertion

--- Modules/Pharma/Tests/Unit/InventoryContractTest.php
+++ Modules/Pharma/Tests/Unit/InventoryContractTest.php
@@ -458,7 +458,7 @@ class InventoryContractTest extends TestCase
         $this->assertStringContainsString('issues/{issue}/bid-sale-edit', $routes);
         $this->assertStringContainsString('Sửa đơn hàng thầu', $documents);
         $this->assertStringContainsString("\$doc->status === 'draft' && (\$doc->issue_source ?? 'normal') !== 'bid'", $documents);
-        $this->assertStringContainsString("!($type === 'issue' && ($doc->issue_source ?? 'normal') === 'bid')", $documents);
+        $this->assertStringContainsString('!($type === \'issue\' && ($doc->issue_source ?? \'normal\') === \'bid\')', $documents);
         $this->assertStringContainsString('DrugBidAwardManagementAssignment::query()', $controller);
         $this->assertStringNotContainsString("with(['partner','award.medicine','managementAssignments.user'])", $controller);
         $bidEdit=file_get_contents(base_path('Modules/Pharma/resources/views/pages/inventory/bid-sale-edit.blade.php'));
