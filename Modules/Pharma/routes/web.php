907de786419cf2d68de62d7e08277335c6af3ebf
tungocvan <tungocvan@gmail.com>
2026-09-24 10:09:25 +0700

    feat(pharma): route bid sale batch approval

--- Modules/Pharma/routes/web.php
+++ Modules/Pharma/routes/web.php
@@ -111,6 +111,8 @@ Route::prefix('admin/pharma')->name('admin.pharma.')->middleware(['web', 'auth:a
         Route::get('/issues/create', [InventoryController::class, 'createIssue'])->middleware('can:create_pharma')->name('issues.create');
         Route::post('/issues', [InventoryController::class, 'storeIssue'])->middleware('can:create_pharma')->name('issues.store');
         Route::get('/issues/export', [InventoryController::class, 'exportIssues'])->name('issues.export');
+        Route::get('/issues/{issue}/bid-sale-batches', [InventoryController::class, 'bidSaleBatches'])->middleware('can:edit_pharma')->name('issues.bid-sales.batches');
+        Route::post('/issues/{issue}/bid-sale-post', [InventoryController::class, 'postBidSaleIssue'])->middleware('can:edit_pharma')->name('issues.bid-sales.post');
         Route::get('/issues/{issue}/pdf', [InventoryController::class, 'issuePdf'])->name('issues.pdf');
         Route::get('/issues/{issue}/print', [InventoryController::class, 'issuePrint'])->name('issues.print');
         Route::get('/issues/{issue}', [InventoryController::class, 'showIssue'])->name('issues.show');
