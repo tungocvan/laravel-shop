0b9961521eea3470429c12613cedf05bb9df2cbd
tungocvan <tungocvan@gmail.com>
2026-09-24 13:40:01 +0700

    feat(pharma): expose warehouse opening cutoff

--- Modules/Pharma/Models/InventoryWarehouse.php
+++ Modules/Pharma/Models/InventoryWarehouse.php
@@ -2,5 +2,7 @@
 namespace Modules\Pharma\Models;
 use Illuminate\Database\Eloquent\Model;
 class InventoryWarehouse extends Model {
- protected $table='pharma_inventory_warehouses'; protected $fillable=['code','name','is_active']; protected $casts=['is_active'=>'boolean'];
+ protected $table='pharma_inventory_warehouses';
+ protected $fillable=['code','name','is_active','opening_cutoff_at'];
+ protected $casts=['is_active'=>'boolean','opening_cutoff_at'=>'datetime'];
 }
