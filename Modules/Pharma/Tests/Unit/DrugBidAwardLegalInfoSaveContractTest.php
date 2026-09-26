3afaead68e752aa335ad42ee14eee4feaf2ea007
tungocvan <tungocvan@gmail.com>
2026-09-26 08:00:34 +0700

    test(pharma): guard bid group contract duration hydration

--- Modules/Pharma/Tests/Unit/DrugBidAwardLegalInfoSaveContractTest.php
+++ Modules/Pharma/Tests/Unit/DrugBidAwardLegalInfoSaveContractTest.php
@@ -23,4 +23,20 @@ class DrugBidAwardLegalInfoSaveContractTest extends TestCase
         $this->assertStringNotContainsString("'unit_price' =>", $editValidation);
         $this->assertStringNotContainsString("'winning_company_name' =>", $editValidation);
     }
+
+    public function test_edit_hydrates_contract_duration_from_result_group_and_index_has_no_hssp_filter(): void
+    {
+        $root = dirname(__DIR__, 2);
+        $component = file_get_contents($root.'/Livewire/DrugBidAward/Form.php');
+        $service = file_get_contents($root.'/Services/DrugBidAwardService.php');
+        $index = file_get_contents($root.'/Livewire/DrugBidAward/Index.php');
+        $view = file_get_contents($root.'/resources/views/livewire/drug-bid-award/index.blade.php');
+
+        $this->assertStringContainsString('legalInfoForResultGroup', $service);
+        $this->assertStringContainsString("\$legalInfo = \$service->legalInfoForResultGroup(\$id)", $component);
+        $this->assertStringContainsString("\$this->contract_duration_months = \$legalInfo->contract_duration_months", $component);
+        $this->assertStringNotContainsString('filterMatchStatus', $index);
+        $this->assertStringNotContainsString('Đối soát HSSP<select', $view);
+    }
+
 }
